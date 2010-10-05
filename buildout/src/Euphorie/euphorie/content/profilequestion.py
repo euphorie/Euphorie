6b89fe8f57e90503dce0b8c4aa8f37600a281d3c
Alexander Pilz <pilz@syslab.com>
2010-10-05 09:50:35 +0000

    changed translation according to #1611

--- buildout/src/Euphorie/euphorie/content/profilequestion.py
+++ buildout/src/Euphorie/euphorie/content/profilequestion.py
@@ -34,10 +34,11 @@ class IProfileQuestion(form.Schema, IRichDescription, IBasic):
     question = schema.TextLine(
             title = _("label_profilequestion_question", default=u"Question"),
             description = _("help_profilequestion_question",
-                default=u"If this will be an optional question, it must be "
-                         "put as a question and be answerable with YES or NO."
-                         "If this will be a repeatable question, it must be"
-                         "put as a prompt to fill in multiple values."),
+                default=u"If this is to be an \"optional\" profile question, "
+                        u"it must be formulated as a question and be answerable "
+                        u"with YES or NO. If this is to be a \"repeatable\" profile "
+                        u"question (statement), it must be formulated as a prompt "
+                        u"to fill in multiple values."),
             required = True)
     form.widget(question="euphorie.content.profilequestion.TextSpan7")
     form.order_after(question="title")
