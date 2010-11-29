786200e470929215bc0a3d41dd4ca9d85f7f5cfe
Wichert Akkerman <wichert@wiggy.net>
2010-11-29 10:46:26 +0000

    Copy reify decorator over from BFG

---
+++ buildout/src/Euphorie/euphorie/tests/test_decorators.py
@@ -0,0 +1,32 @@
+import unittest
+
+class TestReify(unittest.TestCase):
+    def reify(self, wrapped):
+        from euphorie.decorators import reify
+        return reify(wrapped)
+
+    def test___get__withinst(self):
+        def wrapped(inst):
+            return 'a'
+        decorator = self.reify(wrapped)
+        inst = Dummy()
+        result = decorator.__get__(inst)
+        self.assertEqual(result, 'a')
+        self.assertEqual(inst.__dict__['wrapped'], 'a')
+
+    def test___get__noinst(self):
+        decorator = self.reify(None)
+        result = decorator.__get__(None)
+        self.assertEqual(result, decorator)
+
+    def test___doc__copied(self):
+        def wrapped(inst):
+            """My doc"""
+        decorator = self.reify(wrapped)
+        self.assertEqual(decorator.__doc__, "My doc")
+        
+
+
+class Dummy(object):
+    pass
+
