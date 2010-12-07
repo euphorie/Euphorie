3909b0d0f0f003db972d50d8fb3758eeb2091cb2
Wichert Akkerman <wichert@wiggy.net>
2010-12-07 13:16:22 +0000

    Fix version

--- buildout/src/Euphorie/docs/conf.py
+++ buildout/src/Euphorie/docs/conf.py
@@ -45,9 +45,9 @@ copyright = '2009-2010, Simplon'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '2.0'
+version = '2.2'
 # The full version, including alpha/beta/rc tags.
-release = '2.0'
+release = '2.2'
 
 # There are two options for replacing |today|: either, you set today to
 # some non-false value, then it is used:
