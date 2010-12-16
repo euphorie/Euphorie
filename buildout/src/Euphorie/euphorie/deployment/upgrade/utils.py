a5f8b3c238f25f899aaf4a0ed59f7861529b5e5f
Wichert Akkerman <wichert@wiggy.net>
2010-12-16 13:20:33 +0000

    Remove fragile and unused code

--- buildout/src/Euphorie/euphorie/deployment/upgrade/utils.py
+++ buildout/src/Euphorie/euphorie/deployment/upgrade/utils.py
@@ -19,30 +19,3 @@ def ColumnExists(session, table, column):
     return column in table.c
 
 
-def AddColumn(session, klass, col):
-    from sqlalchemy.sql.expression import column
-    from sqlalchemy import schema
-    from sqlalchemy import util
-
-    column_clause=column(col.key, col.type)
-    compiler=session.bind.dialect.ddl_compiler(session.bind.dialect, column_clause)
-    command=["ALTER TABLE", '"%s"' % klass.__tablename__, "ADD COLUMN"]
-    command.append(compiler.get_column_specification(col))
-    for constraint in col.constraints:
-        command.append(compiler.process(constraint))
-
-    if "DEFAULT" not in command[-1]:
-        if col.type.__class__.__name__=="Boolean":
-            command.append("DEFAULT 'f'")
-        else:
-            command.append("DEFAULT ''")
-
-    session.execute(" ".join(command))
-
-    if col.type.__class__.__name__=="Enum":
-        constraint=schema.CheckConstraint(col.in_(col.type.enums), name=col.name,
-                _create_rule=util.portable_instancemethod(col.type._should_create_constraint))
-        constraint.parent=klass.__table__
-        command=["ALTER TABLE", '"%s"' % klass.__tablename__, "ADD ", compiler.process(constraint)]
-        session.execute(" ".join(command))
-
