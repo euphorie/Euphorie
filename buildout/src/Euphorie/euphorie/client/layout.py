a22e32fcad3fac24388213eaa0c609d1bbec13de
Wichert Akkerman <wichert@wiggy.net>
2011-03-28 07:52:42 +0000

    Add a barely usable layout template.

---
+++ buildout/src/Euphorie/euphorie/client/layout.py
@@ -0,0 +1,12 @@
+from zope.interface import Interface
+from five import grok
+from euphorie.client.interfaces import IClientSkinLayer
+
+grok.templatedir("templates")
+
+class Layout(grok.View):
+    grok.context(Interface)
+    grok.name("layout")
+    grok.layer(IClientSkinLayer)
+    grok.template("layout")
+
