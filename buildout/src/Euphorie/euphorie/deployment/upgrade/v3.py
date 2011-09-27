9426cddeec75fee9f1c42c8af17ae1fd56ede3fd
JC Brand <jc@opkode.com>
2011-09-27 14:39:03 +0200

    Remove the renew_survey_published_date upgrade step.
    This step is now in osha.oira

--- buildout/src/Euphorie/euphorie/deployment/upgrade/v3.py
+++ buildout/src/Euphorie/euphorie/deployment/upgrade/v3.py
@@ -1,11 +1,7 @@
-import datetime
 import logging
 from htmllaundry import StripMarkup
-from zope.app.component.hooks import getSite
 from Products.CMFCore.utils import getToolByName
-from euphorie.client.sector import IClientSector
 from euphorie.content.risk import EnsureInterface
-from euphorie.content.survey import ISurvey
 
 log = logging.getLogger(__name__)
 
@@ -54,31 +50,4 @@ def add_wp_column_to_company(context):
         transaction.get().commit()
 
     log.info("Added new column 'workers_participated' to table 'company'")
-
-
-def renew_survey_published_date(context):
-    """ Update the published attr of surveys to set the date to now.
-        This will force all surveys to redirect to the @@update page from where
-        users' session trees can be updated.
-    """
-    site = getSite()
-    client = getattr(site, 'client')
-    # Loop through all client surveys
-    for country in client.objectValues():
-        for sector in country.objectValues():
-            if not IClientSector.providedBy(sector):
-                continue
-
-            for survey in sector.objectValues():
-                if not ISurvey.providedBy(survey):
-                    continue
-
-                published = getattr(survey, "published", None)
-                if isinstance(published, tuple):
-                    survey.published = (
-                        published[0], published[1], datetime.datetime.now())
-                else:
-                    # BBB: Euphorie 1.x did not use a tuple to store extra 
-                    # information.
-                    published = datetime.datetime.now()
 
