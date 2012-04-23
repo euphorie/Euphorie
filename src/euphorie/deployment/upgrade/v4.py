7c7c5e26911147f4a296db8b3016411d1a8c9f32
Wichert Akkerman <wichert@wiggy.net>
2012-04-23 10:12:38 +0200

    Remove unneeded migration step.

--- src/euphorie/deployment/upgrade/v4.py
+++ src/euphorie/deployment/upgrade/v4.py
@@ -1,22 +1,10 @@
 import logging
 from Acquisition import aq_parent
 from euphorie.client.api.authentication import EuphorieAPIPlugin
-from euphorie.client.api.entry import API
 from euphorie.client.setuphandlers import add_api_authentication_plugin
 
 
 log = logging.getLogger(__name__)
-
-
-def add_client_api(context):
-    siteroot = aq_parent(context)
-    client = siteroot.client
-    if 'api' in client:
-        log.info('Client API already configured.')
-        return
-
-    log.info('Enabling client API.')
-    client['api'] = API('api')
 
 
 def add_api_authentication(context):
