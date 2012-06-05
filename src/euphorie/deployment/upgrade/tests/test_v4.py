bcfc5ac38ee718e383712906eb968d37658cd592
Wichert Akkerman <wichert@wiggy.net>
2012-06-05 14:35:08 +0200

    Remove tests for removed code.

--- src/euphorie/deployment/upgrade/tests/test_v4.py
+++ src/euphorie/deployment/upgrade/tests/test_v4.py
@@ -2,20 +2,6 @@ from euphorie.client.tests.database import DatabaseTests
 from euphorie.deployment.tests.functional import EuphorieTestCase
 
 
-class add_api_authentication_tests(EuphorieTestCase):
-    def add_api_authentication(self):
-        from euphorie.deployment.upgrade.v4 import add_api_authentication
-        add_api_authentication(self.portal.portal_setup)
-
-    def test_already_configured(self):
-        self.add_api_authentication()
-
-    def test_plugin_missing(self):
-        self.portal.acl_users.manage_delObjects('euphorie_api')
-        self.add_api_authentication()
-        self.assertTrue('euphorie_api' in self.portal.acl_users.objectIds())
-
-
 class allow_empty_password_tests(DatabaseTests):
     create_tables = False
 
