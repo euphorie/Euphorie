97c1a0bd78f7f40e32b839804200bf14969ae24c
JC Brand <jc@opkode.com>
2012-08-24 17:24:17 +0200

    Remove irrelevant comment.

--- src/euphorie/deployment/upgrade/v5.py
+++ src/euphorie/deployment/upgrade/v5.py
@@ -5,8 +5,6 @@ from Products.CMFCore.utils import getToolByName
 log = logging.getLogger(__name__)
 
 def reindex_solution_titles(context):
-    # XXX: Make sure that all Dexterity types have UIDs before running this
-    # step. There is an upgrade step for plone.app.dexterity that does this.
     catalog = getToolByName(context, 'portal_catalog')
     ps = catalog(title='Hello World', portal_type='euphorie.solution')
     for p in ps:
