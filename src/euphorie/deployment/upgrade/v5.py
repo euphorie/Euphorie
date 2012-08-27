22c256c8a1ab55d0e018e063cebd25a095a7a1ee
JC Brand <jc@opkode.com>
2012-08-27 19:04:28 +0200

    Add upgrade step to reindex IRichDescriptions

--- src/euphorie/deployment/upgrade/v5.py
+++ src/euphorie/deployment/upgrade/v5.py
@@ -1,13 +1,28 @@
 import logging
 import transaction
 from Products.CMFCore.utils import getToolByName
+from euphorie.content.behaviour.richdescription import IRichDescription
 
 log = logging.getLogger(__name__)
 
 def reindex_solution_titles(context):
     catalog = getToolByName(context, 'portal_catalog')
-    ps = catalog(title='Hello World', portal_type='euphorie.solution')
+    ps = catalog(title='title_common_solution', portal_type='euphorie.solution')
+    i = 0
     for p in ps:
         obj = p.getObject()
         obj.reindexObject('Title')
-        transaction.commit()
+        i += 1
+        if i == 100:
+            transaction.commit()
+
+def reindex_richtext_descriptions(context):
+    catalog = getToolByName(context, 'portal_catalog')
+    ps = catalog(object_provides=IRichDescription.__identifier__)
+    i = 0
+    for p in ps:
+        obj = p.getObject()
+        obj.reindexObject('Description')
+        i += 1
+        if i == 100:
+            transaction.commit()
