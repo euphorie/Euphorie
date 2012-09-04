b1a838a9e7afdacf550eb7771ba83556f3e93dfa
Alexander Pilz <pilz@syslab.com>
2012-09-04 09:34:01 +0200

    trying to remove placeholder classes

--- src/euphorie/client/templates/behaviour/plan.js
+++ src/euphorie/client/templates/behaviour/plan.js
@@ -82,7 +82,7 @@ var ActionPlan = {
         $new_tab.addClass("current");
         $("#ActionPlanItemForm .tab-container").removeClass("current").hide();
         $new_container.addClass("current").show('fast', $.proxy(function() {
-                $new_container.find("textarea").removeClass("placeholder");
+                this.find("textarea").removeClass("placeholder");
                 this.placeholder();
         }, $new_container));
         ActionPlan.UpdateNumbering();
