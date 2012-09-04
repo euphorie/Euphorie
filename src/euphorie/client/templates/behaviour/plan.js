8df346bdc8bd4fd7b3f7ac1c40631ae333854f25
Alexander Pilz <pilz@syslab.com>
2012-09-04 09:27:59 +0200

    more placeholder fixing

--- src/euphorie/client/templates/behaviour/plan.js
+++ src/euphorie/client/templates/behaviour/plan.js
@@ -82,12 +82,13 @@ var ActionPlan = {
         $new_tab.addClass("current");
         $("#ActionPlanItemForm .tab-container").removeClass("current").hide();
         $new_container.addClass("current").show('fast', $.proxy(function() {
+                $new_container.find("textarea, input").removeClass("placeholder");
                 this.placeholder();
         }, $new_container));
         ActionPlan.UpdateNumbering();
 
-        $("textarea[placeholder], input[placeholder]").removeClass('placeholder');
-        $("textarea[placeholder], input[placeholder]").placeholder();
+        /*$("textarea[placeholder], input[placeholder]").removeClass('placeholder');
+        $("textarea[placeholder], input[placeholder]").placeholder();*/
         ActionPlan.chevronize();
     },
 
