ce865782f7457c4297288d337149d33c585bcd83
Wichert Akkerman <wichert@wiggy.net>
2012-09-27 12:16:02 +0200

    Fix failing test.

--- src/euphorie/content/tests/test_functional_solution.py
+++ src/euphorie/content/tests/test_functional_solution.py
@@ -1,21 +1,24 @@
 from zope.app.component.hooks import getSite
 from euphorie.deployment.tests.functional import EuphorieTestCase
 
+
 def _create(container, *args, **kwargs):
     newid = container.invokeFactory(*args, **kwargs)
     return getattr(container, newid)
+
 
 def createSolution(algorithm=u'kinney'):
     portal = getSite()
     country = portal.sectors.nl
     sector = _create(country, "euphorie.sector", "sector")
     surveygroup = _create(sector, "euphorie.surveygroup", "group",
-            evaluation_algorithm = algorithm)
+            evaluation_algorithm=algorithm)
     survey = _create(surveygroup, "euphorie.survey", "survey")
     module = _create(survey, "euphorie.module", "module")
     risk = _create(module, "euphorie.risk", "risk")
     solution = _create(risk, "euphorie.solution", "solution")
     return solution
+
 
 class RiskTests(EuphorieTestCase):
 
@@ -32,11 +35,14 @@ class RiskTests(EuphorieTestCase):
     def testCMFAccessors(self):
         """ CMF-Style accessors must return utf8-encoded strings.
         """
+        from zope.i18n import translate
+        from euphorie.content import utils
         from euphorie.content.solution import Solution
         self.loginAsPortalOwner()
         solution = createSolution()
         title = solution.Title()
         self.assertEqual(type(title), str)
-        self.assertEqual(title, Solution.title.encode('utf-8'))
-
-
+        survey = utils.getSurvey(solution)
+        self.assertEqual(
+                title,
+                translate(Solution.title, target_language=survey.language))
