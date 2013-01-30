0e052df95fb7225c57c027de2916245db24a0011
JC Brand <jc@opkode.com>
2013-01-30 11:29:11 +0200

    Add layer otherwise plone.app.search's browserview gets used

--- src/euphorie/deployment/browser/search.py
+++ src/euphorie/deployment/browser/search.py
@@ -1,6 +1,7 @@
 from zope.interface import Interface
 from five import grok
 from Products.CMFCore.utils import getToolByName
+from plonetheme.nuplone.skin.interfaces import NuPloneSkin
 
 grok.templatedir("templates")
 
@@ -21,6 +22,7 @@ class Search(grok.View):
     grok.name("search")
     grok.require("zope2.View")
     grok.template("search")
+    grok.layer(NuPloneSkin)
 
     def update(self):
         query = self.request.form.get("q", None)
