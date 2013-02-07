6043fd9637fc783b65403b7b3e6c708216389ac5
Wichert Akkerman <wichert@wiggy.net>
2013-02-07 13:55:54 +0100

    Cleanup markup.js.

--- src/euphorie/client/templates/behaviour/markup.js
+++ src/euphorie/client/templates/behaviour/markup.js
@@ -4,86 +4,61 @@
  * Copyright 2008-2013 Simplon B.V.
  */
 
-function GetBrowserVersion() {
-    return Number( $.browser.version.split(".", 2).join(""));
-}
+(function($) {
+        $(".jsOnly").show();
 
-function GetBrowserEngine() {
-    var i, engines = [ "safari", "opera", "msie", "mozilla" ];
-    for (i=0; i<engines.length; i++) {
-        if ($.browser[engines[i]]) {
-            return engines[i];
-        }
-    }
-}
-
-var engine = GetBrowserEngine();
-var engine_version = GetBrowserVersion();
-var iphone = (navigator.userAgent.search("iPhone")!==-1);
-
-
-$(".printButton").on("click", function() {
-    window.print();
-    return false;
-});
-
-
-$(document).ready(function() {
-    $(".jsOnly").show();
-
-    var tmp;
-
-    // Numeric input fields
-    $("input.numeric").numeric();
-
-    // PNG transparancy fix for MS Internet Explorer 6
-    if (engine==="msie" && engine_version<70) {
-        var fixImage = function(img) {
-            $(img).css({
-                    height: "0px",
-                    width: $(img).width()+"px",
-                    "padding-top" : $(img) .height()+"px",
-                    overflow : "hidden",
-                    filter : "progid:DXImageTransform.Microsoft.AlphaImageLoader(src='" + $(img).attr("src") + "', sizingMethod='image')"
-                    });
-        };
-
-        $("img[src$=.png]").each(function() {
-            fixImage(this);
+        $(".printButton").on("click", function() {
+            window.print();
+            return false;
         });
-    }
 
-    $("a[rel=fancybox]").fancybox({
+        $("input.numeric").numeric();
+
+
+        $("a[rel=fancybox]").fancybox({
             "transitionIn"    : "elastic",
             "transitionOut"   : "elastic",
             "titlePosition"   : "over"
             });
 
-});
+        if ($.browser.msie) {
+            var browser_version = Number($.browser.version.split(".", 1)[0]);
+            // PNG transparancy fix for MS Internet Explorer 6
+            if (browser_version<7) {
+                var fixImage = function(img) {
+                    $(img).css({
+                            height: "0px",
+                            width: $(img).width()+"px",
+                            "padding-top" : $(img) .height()+"px",
+                            overflow : "hidden",
+                            filter : "progid:DXImageTransform.Microsoft.AlphaImageLoader(src='" + $(img).attr("src") + "', sizingMethod='image')"
+                            });
+                };
 
+                $("img[src$=.png]").each(function() {
+                    fixImage(this);
+                });
+            }
 
-// Work around broken button behaviour for IE 6 and 7
-if (engine==="msie" && engine_version<80) {
-    $("form button[type=submit]").live("click", function() {
-        var name = $(this).attr("name"),
-            value = $(this).attr("class").split(" ")[0];
+            // Work around broken button behaviour for IE 6 and 7
+            if (browser_version<8) {
+                $("form button[type=submit]").live("click", function() {
+                    var name = $(this).attr("name"),
+                        value = $(this).attr("class").split(" ")[0];
 
-        $("<input type='hidden'/>")
-            .attr("name", name)
-            .attr("value", value)
-            .appendTo(this.form);
-        $("button[type=submit]", this.form).attr("name", "_buttonfix");
-    });
-}
+                    $("<input type='hidden'/>")
+                        .attr("name", name)
+                        .attr("value", value)
+                        .appendTo(this.form);
+                    $("button[type=submit]", this.form).attr("name", "_buttonfix");
+                });
+            }
+        }
 
-// iPhone hackey
-if (iphone) {
-    var updateLayout = function() {
-        window.scrollTo(0, 1);
-    };
-
-    $(document).ready(function() {
-        setTimeout(updateLayout, 100);
-    });
-}
-
+        // iPhone hackery
+        if (navigator.userAgent.search("iPhone")!==-1) {
+            setTimeout(function() {
+                    window.scrollTo(0, 1);
+                }, 100);
+        }
+})(jQuery);
