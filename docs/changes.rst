d14f3ab20310b97a3c5cfea2e4be4f13858259e8
Wolfgang Thomas <thomas@syslab.com>
2013-04-23 11:41:51 +0200

    Back to development: 6.0rc4

--- docs/changes.rst
+++ docs/changes.rst
@@ -1,6 +1,12 @@
 Changelog
 =========
 
+6.0rc4 (unreleased)
+-------------------
+
+- Nothing changed yet.
+
+
 6.0rc3 (2013-04-23)
 -------------------
 
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '6.0rc3'
+version = '6.0rc4.dev0'
 
 setup(name="Euphorie",
       version=version,
