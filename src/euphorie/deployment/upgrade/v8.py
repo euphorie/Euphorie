0cc8e52a84df937ff59b6d971ef7c4baf4fb772c
JC Brand <jc@opkode.com>
2014-07-08 16:40:42 +0200

    Add a custom datamanager that hashes passwords before storing them
    OSHA refs #10285

---
+++ src/euphorie/deployment/upgrade/v8.py
@@ -0,0 +1,33 @@
+# -*- coding: UTF-8 -*-
+from plone import api
+from plone.dexterity import utils
+from z3c.form.interfaces import IDataManager
+from euphorie.content.user import IUser
+import logging
+import zope.component
+
+log = logging.getLogger(__name__)
+
+
+def hash_passwords(context):
+    """ Make sure IUser passwords are hashed before they're stored in the ZODB.
+    """
+    catalog = api.portal.get_tool('portal_catalog')
+    ps = catalog(object_provides='euphorie.content.user.IUser')
+    for p in ps:
+        o = p.getObject()
+        password = o.password
+        if type(password) == str and len(password) == 60:
+            log.info('Not hashing password for "%s". Appears to be hashed '
+                     'already.' %  p.getPath())
+            continue
+        elif password is None:
+            log.info('Not hashing password for "%s". No password set'
+                    %  p.getPath())
+            continue
+
+        for schema in utils.iterSchemata(o):
+            field = schema.get('password')
+            if field and field.interface == IUser:
+                dm = zope.component.getMultiAdapter(
+                    (o, field), IDataManager).set(password)
