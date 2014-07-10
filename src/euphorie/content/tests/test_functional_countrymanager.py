b535746ff7121045ed3c5a4038bf0a0d749c5748
JC Brand <jc@opkode.com>
2014-07-10 17:19:52 +0200

    Add a test to check that country manager's pw is hashed
    OSHA refs #10285

--- src/euphorie/content/tests/test_functional_countrymanager.py
+++ src/euphorie/content/tests/test_functional_countrymanager.py
@@ -1,4 +1,8 @@
 from euphorie.deployment.tests.functional import EuphorieTestCase
+from plone.dexterity.interfaces import IDexterityFTI
+from z3c.form.interfaces import IDataManager
+from zope import component
+import bcrypt
 
 
 class CountryManagerTests(EuphorieTestCase):
@@ -12,3 +16,16 @@ class CountryManagerTests(EuphorieTestCase):
         self.loginAsPortalOwner()
         manager = self.createCountryManager()
         self.assertFalse(manager.cb_isCopyable())
+
+    def testPasswordsAreHashed(self):
+        self.loginAsPortalOwner()
+        manager = self.createCountryManager()
+
+        fti = component.queryUtility(IDexterityFTI, name=manager.portal_type)
+        field =  fti.lookupSchema().get('password')
+        dm = component.getMultiAdapter(
+            (manager, field), IDataManager).set('secret')
+
+        self.assertFalse(manager.password == 'secret')
+        self.assertTrue(
+                bcrypt.hashpw('secret', manager.password) == manager.password)
