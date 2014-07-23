c5241e094ac6dc0a51c3d80b75de30a71d00f8d7
JC Brand <jc@opkode.com>
2014-07-23 12:23:49 +0200

    Add a custom password policy

--- src/euphorie/content/passwordpolicy.py
+++ src/euphorie/content/passwordpolicy.py
@@ -1,3 +1,4 @@
+import re
 from AccessControl import ClassSecurityInfo
 from App.class_init import InitializeClass
 from Products.PlonePAS.plugins import passwordpolicy
@@ -18,18 +19,31 @@ class EuphoriePasswordPolicy(passwordpolicy.PasswordPolicyPlugin):
     def validateUserInfo(self, user, set_id, set_info ):
         """ See IValidationPlugin. Used to validate password property
         """
-        # FIXME: still using the stock Plone policy
-
         if not set_info:
             return []
         password = set_info.get('password', None)
         if password is None:
             return []
-        elif password == '':
-            return [{'id':'password','error':_(u'Minimum 5 characters.')}]
-        elif len(password) < 5:
-            return [{'id':'password','error':
-                _(u'Your password must contain at least 5 characters.')}]
+
+        failed = False
+        if len(password) < 5:
+            failed = True
+        elif len([l for l in password if l.isupper()]) == 0:
+            # Must have capital letter(s)
+            failed = True
+        elif not re.match("[1-9]"):
+            # Must have numbers(s)
+            failed = True
+        elif not re.match("[^a-zA-Z1-9"):
+            # Must have special chars (i.e. not alphanumerical)
+            failed = True
+
+        if failed:
+            return [{'id':'password','error': _(
+                u"Your password must contain at least 5 characters, "
+                u"including at least one capital letter, one number and "
+                u"one special character (e.g. $, # or @')."
+            )}]
         else:
             return []
 
