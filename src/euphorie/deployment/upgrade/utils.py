0dafa89ac296eb6c320a91c36a7b11dded9c5779
Wolfgang Thomas <thomas@syslab.com>
2016-06-02 09:43:49 +0200

    new parameter is required

--- src/euphorie/deployment/upgrade/utils.py
+++ src/euphorie/deployment/upgrade/utils.py
@@ -13,7 +13,7 @@ def ColumnExists(session, table, column):
     metadata = MetaData(connection)
     table = Table(table, metadata)
     try:
-        connection.dialect.reflecttable(connection, table, None)
+        connection.dialect.reflecttable(connection, table, None, exclude_columns=tuple())
     except NoSuchTableError:
         return False
     return column in table.c
