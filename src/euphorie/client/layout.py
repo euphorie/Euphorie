d6f0826301c62719caf53e5dd36336432b5de410
Wolfgang Thomas <thomas@syslab.com>
2016-06-23 12:20:13 +0200

    added missing tooltips

--- src/euphorie/client/layout.py
+++ src/euphorie/client/layout.py
@@ -30,3 +30,15 @@ class Includes(grok.View):
     grok.name("includes")
     grok.layer(IClientSkinLayer)
     grok.template("includes")
+
+
+class Tooltips(grok.View):
+    """ This view's templates contains a number of <div> element that are used
+    for various tooltips.
+    In proto, see explanations.html
+
+    """
+    grok.context(Interface)
+    grok.name("tooltips")
+    grok.layer(IClientSkinLayer)
+    grok.template("tooltips")
