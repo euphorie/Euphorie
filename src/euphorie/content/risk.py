1f17984032aa5ef5bfa64c4fcb0e277b6f65d173
Wolfgang Thomas <thomas@syslab.com>
2017-07-20 13:49:02 +0200

    added missing messageid

--- src/euphorie/content/risk.py
+++ src/euphorie/content/risk.py
@@ -83,7 +83,8 @@ class IRisk(form.Schema, IRichDescription, IBasic):
                 u'next to the statement in the client. Leave blank if no '
                 u'such sentence is needed'),
         vocabulary=SimpleVocabulary([
-                        SimpleTerm(u"", title="-- no selection --"),
+                        SimpleTerm(u"",
+                            title=_("-- no selection --")),
                         SimpleTerm(u"sufficient",
                             title=_("title_extra_sufficient",
                             default=u"Existing measures are sufficient.")),
