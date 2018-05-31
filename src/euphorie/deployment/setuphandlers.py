625cca8a89383878bd5bfac5dae6fa5145b55e90
ale-rt <alessandro.pisa@gmail.com>
2018-05-31 16:20:20 +0200

    cleanup

--- src/euphorie/deployment/setuphandlers.py
+++ src/euphorie/deployment/setuphandlers.py
@@ -16,6 +16,7 @@ import logging
 
 log = logging.getLogger(__name__)
 
+
 def setupVarious(context):
     site = api.portal.get()
     disableRedirectTracking(site)
@@ -25,50 +26,46 @@ def setupVarious(context):
 
 
 COUNTRIES = {
-        "at": (u"Austria", "eu-member"),
-        "be": (u"Belgium", "eu-member"),
-        "bg": (u"Bulgaria", "eu-member"),
-        "se": (u"Sweden", "eu-member"),
-        "cy": (u"Cyprus", "eu-member"),
-        "cz": (u"The Czech Republic", "eu-member"),
-        "de": (u"Germany", "eu-member"),
-        "dk": (u"Denmark", "eu-member"),
-        "ee": (u"Estonia", "eu-member"),
-        "es": (u"Spain", "eu-member"),
-        "fi": (u"Finland", "eu-member"),
-        "fr": (u"France", "eu-member"),
-        "gb": (u"The United Kingdom", "eu-member"),
-        "gr": (u"Greece", "eu-member"),
-        "hu": (u"Hungary", "eu-member"),
-        "ie": (u"Ireland", "eu-member"),
-        "it": (u"Italy", "eu-member"),
-        "lt": (u"Lithuania", "eu-member"),
-        "lu": (u"Luxembourg", "eu-member"),
-        "lv": (u"Latvia", "eu-member"),
-        "mt": (u"Malta", "eu-member"),
-        "nl": (u"The Netherlands", "eu-member"),
-        "pl": (u"Poland", "eu-member"),
-        "pt": (u"Portugal", "eu-member"),
-        "ro": (u"Romania", "eu-member"),
-        "sk": (u"Slovakia", "eu-member"),
-        "si": (u"Slovenia", "eu-member"),
-
-        "li": (u"Liechtenstein", "efta"),
-        "no": (u"Norway", "efta"),
-        "ch": (u"Switzerland", "efta"),
-
-        "hr": (u"Republic of Croatia", "candidate-eu"),
-        "is": (u"Republic of Iceland", "candidate-eu"),
-        "mk": (u"F.Y.R. Macedonia", "candidate-eu"),
-        "me": (u"Montenegro", "candidate-eu"),
-        "tr": (u"Republic of Turkey", "candidate-eu"),
-
-        "al": (u"Republic of Albania", "potential-candidate-eu"),
-        "ba": (u"Bosnia and Herzegovina", "potential-candidate-eu"),
-        "cs": (u"Kosovo", "potential-candidate-eu"),
-        "rs": (u"Republic of Serbia", "potential-candidate-eu"),
-        }
-
+    "at": (u"Austria", "eu-member"),
+    "be": (u"Belgium", "eu-member"),
+    "bg": (u"Bulgaria", "eu-member"),
+    "se": (u"Sweden", "eu-member"),
+    "cy": (u"Cyprus", "eu-member"),
+    "cz": (u"The Czech Republic", "eu-member"),
+    "de": (u"Germany", "eu-member"),
+    "dk": (u"Denmark", "eu-member"),
+    "ee": (u"Estonia", "eu-member"),
+    "es": (u"Spain", "eu-member"),
+    "fi": (u"Finland", "eu-member"),
+    "fr": (u"France", "eu-member"),
+    "gb": (u"The United Kingdom", "eu-member"),
+    "gr": (u"Greece", "eu-member"),
+    "hu": (u"Hungary", "eu-member"),
+    "ie": (u"Ireland", "eu-member"),
+    "it": (u"Italy", "eu-member"),
+    "lt": (u"Lithuania", "eu-member"),
+    "lu": (u"Luxembourg", "eu-member"),
+    "lv": (u"Latvia", "eu-member"),
+    "mt": (u"Malta", "eu-member"),
+    "nl": (u"The Netherlands", "eu-member"),
+    "pl": (u"Poland", "eu-member"),
+    "pt": (u"Portugal", "eu-member"),
+    "ro": (u"Romania", "eu-member"),
+    "sk": (u"Slovakia", "eu-member"),
+    "si": (u"Slovenia", "eu-member"),
+    "li": (u"Liechtenstein", "efta"),
+    "no": (u"Norway", "efta"),
+    "ch": (u"Switzerland", "efta"),
+    "hr": (u"Republic of Croatia", "candidate-eu"),
+    "is": (u"Republic of Iceland", "candidate-eu"),
+    "mk": (u"F.Y.R. Macedonia", "candidate-eu"),
+    "me": (u"Montenegro", "candidate-eu"),
+    "tr": (u"Republic of Turkey", "candidate-eu"),
+    "al": (u"Republic of Albania", "potential-candidate-eu"),
+    "ba": (u"Bosnia and Herzegovina", "potential-candidate-eu"),
+    "cs": (u"Kosovo", "potential-candidate-eu"),
+    "rs": (u"Republic of Serbia", "potential-candidate-eu"),
+}
 
 for i in REGION_NAMES.items():
     COUNTRIES[i[0]] = (i[1], "region")
@@ -82,8 +79,11 @@ def setupInitialContent(site):
             log.info("Removed default Plone %s folder", obj)
 
     if "sectors" not in present:
-        site.invokeFactory("euphorie.sectorcontainer",
-                "sectors", title="Surveys")
+        site.invokeFactory(
+            "euphorie.sectorcontainer",
+            "sectors",
+            title="Surveys",
+        )
         mt = getToolByName(site, "portal_membership")
         mt.setMembersFolderById("sectors")
         log.info("Added sectors folder")
@@ -92,20 +92,33 @@ def setupInitialContent(site):
     for (country_id, info) in COUNTRIES.items():
         (title, country_type) = info
         if country_id not in sectors:
-            sectors.invokeFactory("euphorie.country", country_id,
-                    title=title, country_type=country_type)
+            sectors.invokeFactory(
+                "euphorie.country",
+                country_id,
+                title=title,
+                country_type=country_type,
+            )
             log.info("Added country %s (%s)", country_id, title)
         country = sectors[country_id]
         if "help" not in country:
-            createContentInContainer(country, "euphorie.page", id="help",
-                    title=u"Help", checkConstraints=False)
-            log.info("Added help section for country %s (%s)",
-                    country_id, title)
+            createContentInContainer(
+                country,
+                "euphorie.page",
+                id="help",
+                title=u"Help",
+                checkConstraints=False,
+            )
+            log.info(
+                "Added help section for country %s (%s)", country_id, title
+            )
         help = country["help"]
         if not INavigationRoot.providedBy(help):
             alsoProvides(help, INavigationRoot)
-            log.info("Made help for country %s (%s) a navigation root.",
-                    country_id, title)
+            log.info(
+                "Made help for country %s (%s) a navigation root.",
+                country_id,
+                title,
+            )
 
     if "client" not in present:
         site.invokeFactory("euphorie.client", "client", title="Client")
@@ -129,16 +142,28 @@ def setupInitialContent(site):
     present_languages = documents.objectIds()
     for (code, name) in lt.listSupportedLanguages():
         if code not in present_languages:
-            documents.invokeFactory("euphorie.documentation", code, title=name)
+            documents.invokeFactory(
+                "euphorie.documentation",
+                code,
+                title=name,
+            )
             log.info("Added documentation folder for %s (%s)", name, code)
         docs = documents[code]
         if "help" not in docs:
-            createContentInContainer(docs, "euphorie.help",
-                    id="help", checkConstraints=False)
+            createContentInContainer(
+                docs,
+                "euphorie.help",
+                id="help",
+                checkConstraints=False,
+            )
             log.info("Added online help text for language %s (%s)", name, code)
         if "appendix" not in docs:
-            _createObjectByType("euphorie.page", docs,
-                    "appendix", title=u"Appendix")
+            _createObjectByType(
+                "euphorie.page",
+                docs,
+                "appendix",
+                title=u"Appendix",
+            )
             log.info("Added appendix folder for language %s (%s)", name, code)
 
 
@@ -149,8 +174,10 @@ def disableRedirectTracking(site):
     from plone.app.redirector.interfaces import IRedirectionStorage
     sm = getSiteManager(site)
     if sm is None or not IComponentRegistry.providedBy(sm):
-        log.warn("Failed to find a site manager, can not remove "
-                 "IRedirectionStorage utility")
+        log.warn(
+            "Failed to find a site manager, can not remove "
+            "IRedirectionStorage utility"
+        )
         return
 
     sm.unregisterUtility(provided=IRedirectionStorage)
@@ -169,13 +196,16 @@ def registerPasswordPolicy(site):
     # Deactivate the default policy
     for oid in pas.objectIds([PasswordPolicyPlugin.meta_type]):
         if oid in pas.plugins._getPlugins(IValidationPlugin):
-            pas.plugins.deactivatePlugin(IValidationPlugin, oid)
+            pas.plugins.deactivatePlugin(
+                IValidationPlugin,
+                oid,
+            )
 
     # Activate the Euphorie policy
     if not pas.objectIds([EuphoriePasswordPolicy.meta_type]):
         plugin = EuphoriePasswordPolicy(
             EuphoriePasswordPolicy.id,
-            EuphoriePasswordPolicy.meta_type
+            EuphoriePasswordPolicy.meta_type,
         )
         pas._setObject(plugin.getId(), plugin)
         plugin = getattr(pas, plugin.getId())
@@ -187,4 +217,7 @@ def registerPasswordPolicy(site):
         plugin.manage_activateInterfaces([info["id"] for info in infos])
         for info in infos:
             for i in range(len(pas.plugins.listPluginIds(info["interface"]))):
-                pas.plugins.movePluginsUp(info["interface"], [plugin.getId()])
+                pas.plugins.movePluginsUp(
+                    info["interface"],
+                    [plugin.getId()],
+                )
