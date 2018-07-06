425e19419b82d3c3fd677977087e08d0efd5e854
ale-rt <alessandro.pisa@gmail.com>
2018-07-06 16:56:57 +0200

    Feature toggled message to download the the report for non editors

--- src/euphorie/client/browser/survey.py
+++ src/euphorie/client/browser/survey.py
@@ -47,6 +47,13 @@ class Start(AutoExtensibleForm, EditForm):
             self.request,
         )
 
+    @property
+    @memoize
+    def can_edit(self):
+        ''' Check if a user can edit this session
+        '''
+        return True
+
     @memoize
     def has_introduction(self):
         survey = aq_inner(self.context)
