48145dbcc81ca65a662c488fddf7f65bf7a3ad7f
Wolfgang Thomas <thomas@syslab.com>
2018-08-21 16:46:11 +0200

    Upgrade step: register the default Euphorie content types to appear in the navigation

--- src/euphorie/deployment/upgrade/v20.py
+++ src/euphorie/deployment/upgrade/v20.py
@@ -1,5 +1,6 @@
 # -*- coding: UTF-8 -*-
 from euphorie.client import model
+from plone.app.upgrade.utils import loadMigrationProfile
 from sqlalchemy.engine.reflection import Inspector
 from z3c.saconfig import Session
 from zope.sqlalchemy import datamanager
@@ -19,3 +20,14 @@ def add_column_for_training_notes(context):
             "ALTER TABLE %s ADD training_notes TEXT" %
             model.Risk.__table__.name)
         datamanager.mark_changed(session)
+
+
+def update_nav_types_registry(context):
+    ''' There is a new registry record called
+    plone.displayed_types
+    that determines what types get shown in the navigation
+    '''
+    loadMigrationProfile(
+        context,
+        'profile-euphorie.deployment.upgrade:to_0020'
+    )
