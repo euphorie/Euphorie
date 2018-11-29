d388f4674a94806229be404b274df84b929229ff
Wolfgang Thomas <thomas@syslab.com>
2018-11-29 17:04:21 +0100

    For the description (HTML) text, let's try to create properly formatted output (bold, italics, hyperlink, lists)

--- src/euphorie/client/docx/compiler.py
+++ src/euphorie/client/docx/compiler.py
@@ -15,6 +15,8 @@ from z3c.appconfig.interfaces import IAppConfig
 from zope.component import getUtility
 from zope.i18n import translate
 import htmllaundry
+import lxml.html
+import docx
 
 
 def _escape_text(txt):
@@ -174,6 +176,8 @@ class DocxCompiler(BaseOfficeCompiler):
                         htmllaundry.StripMarkup(description))))
                 )
 
+                doc = HtmlToWord(description, doc)
+
             if node.comment and node.comment.strip():
                 doc.add_paragraph(node.comment, style="Comment")
 
@@ -286,3 +290,104 @@ class DocxCompiler(BaseOfficeCompiler):
         '''
         self.set_session_title_row(data)
         self.set_body(data)
+
+def add_hyperlink_into_run(paragraph, run, url):
+    runs = paragraph.runs
+    for i in range(len(runs)):
+        if runs[i].text == run.text:
+            break
+
+    # This gets access to the document.xml.rels file and gets a new
+    # relation id value
+    part = paragraph.part
+    r_id = part.relate_to(
+        url,
+        docx.opc.constants.RELATIONSHIP_TYPE.HYPERLINK,
+        is_external=True)
+
+    # Create the w:hyperlink tag and add needed values
+    hyperlink = docx.oxml.shared.OxmlElement('w:hyperlink')
+    hyperlink.set(docx.oxml.shared.qn('r:id'), r_id, )
+    hyperlink.append(run._r)
+    paragraph._p.insert(i + 1, hyperlink)
+
+
+class _HtmlToWord(object):
+
+    def handleInlineText(self, node, p, run=None):
+        """Handler for elements which can only contain inline text (p, li)"""
+        if not run:
+            run = p.add_run()
+        font = run.font
+        if node.tag in ["strong", "b"]:
+            font.bold = True
+        elif node.tag in ["em", "i"]:
+            font.italic = True
+        elif node.tag == "u":
+            font.underline = True
+
+        if node.text and node.text.strip():
+            keep_run = False
+            if node.tag == 'a':
+                href = node.get('href')
+                href = href and href.strip()
+                if href and href != node.text.strip():
+                    run.style = "Hyperlink"
+                    run.text = node.text
+                    add_hyperlink_into_run(p, run, href)
+                else:
+                    run.text = node.text
+            else:
+                run.text = node.text
+        else:
+            keep_run = True
+
+        for sub in node:
+            p = self.handleInlineText(sub, p, run=keep_run and run)
+        if node.tail and node.tail.strip():
+            run = p.add_run()
+            run.text = node.tail
+        return p
+
+    def handleElement(self, node, doc):
+        if node.tag in ["p", "li", 'strong', 'b', 'em', 'i', 'u', 'a']:
+            p = doc.add_paragraph()
+            p = self.handleInlineText(node, p)
+        elif node.tag in ["ul", "ol"]:
+
+            if node.tag == "ul":
+                style = "List Bullet"
+            else:
+                style = "List Number"
+            for sub in node:
+                if sub.tag == "li":
+                    p = doc.add_paragraph(style=style)
+                    p = self.handleInlineText(sub, p)
+
+        tail = node.tail
+        # Prevent unwanted empty lines inside listings and paragraphs that come
+        # from newlines in the markup
+        if node.tag in ['li', 'p', 'strong', 'em', 'b', 'i']:
+            tail = tail and tail.strip()
+        if tail:
+            doc.add_paragraph(tail)
+        return doc
+
+    def __call__(self, markup, doc):
+        if not markup or not markup.strip():
+            return []
+        try:
+            markup_doc = lxml.html.document_fromstring(markup)
+        except etree.XMLSyntaxError:
+            text = htmllaundry.StripMarkup(markup)
+            text = text.replace("&#13", "\n")
+            doc.add_paragraph(text)
+            return doc
+
+        for node in markup_doc.find('body'):
+            doc = self.handleElement(node, doc)
+
+        return doc
+
+
+HtmlToWord = _HtmlToWord()
