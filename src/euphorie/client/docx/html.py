d467301cb97887a5de34f91290b184997fb6913e
Wolfgang Thomas <thomas@syslab.com>
2019-01-17 16:01:31 +0100

    provide default value for variable when making a hyperlink

--- src/euphorie/client/docx/html.py
+++ src/euphorie/client/docx/html.py
@@ -7,6 +7,7 @@ from lxml import etree
 
 def add_hyperlink_into_run(paragraph, run, url):
     runs = paragraph.runs
+    i = 0
     for i in range(len(runs)):
         if runs[i].text == run.text:
             break
