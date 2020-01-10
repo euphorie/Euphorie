9235661fd65d5024fdc80aab18416693b2f76c60
Wolfgang Thomas <thomas@syslab.com>
2020-01-10 18:11:43 +0100

    new styles and new bundle

---
+++ src/euphorie/client/resources/bundles/chunks/2-3611aa70791ee2e7501f.js
@@ -0,0 +1,159 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[2],{
+
+/***/ 20:
+/***/ (function(module, exports, __webpack_require__) {
+
+var map = {
+	"./af.js": 122,
+	"./ar-dz.js": 123,
+	"./ar-kw.js": 124,
+	"./ar-ly.js": 125,
+	"./ar-ma.js": 126,
+	"./ar-sa.js": 127,
+	"./ar-tn.js": 128,
+	"./ar.js": 129,
+	"./az.js": 130,
+	"./be.js": 131,
+	"./bg.js": 58,
+	"./bm.js": 132,
+	"./bn.js": 133,
+	"./bo.js": 134,
+	"./br.js": 135,
+	"./bs.js": 136,
+	"./ca.js": 137,
+	"./cs.js": 60,
+	"./cv.js": 138,
+	"./cy.js": 139,
+	"./da.js": 61,
+	"./de-at.js": 140,
+	"./de-ch.js": 141,
+	"./de.js": 65,
+	"./dv.js": 142,
+	"./el.js": 66,
+	"./en-SG.js": 143,
+	"./en-au.js": 144,
+	"./en-ca.js": 145,
+	"./en-gb.js": 146,
+	"./en-ie.js": 147,
+	"./en-il.js": 148,
+	"./en-nz.js": 149,
+	"./eo.js": 150,
+	"./es-do.js": 151,
+	"./es-us.js": 152,
+	"./es.js": 19,
+	"./et.js": 153,
+	"./eu.js": 154,
+	"./fa.js": 155,
+	"./fi.js": 63,
+	"./fo.js": 156,
+	"./fr-ca.js": 157,
+	"./fr-ch.js": 158,
+	"./fr.js": 64,
+	"./fy.js": 159,
+	"./ga.js": 160,
+	"./gd.js": 161,
+	"./gl.js": 162,
+	"./gom-latn.js": 163,
+	"./gu.js": 164,
+	"./he.js": 165,
+	"./hi.js": 166,
+	"./hr.js": 59,
+	"./hu.js": 67,
+	"./hy-am.js": 167,
+	"./id.js": 168,
+	"./is.js": 169,
+	"./it-ch.js": 170,
+	"./it.js": 68,
+	"./ja.js": 171,
+	"./jv.js": 172,
+	"./ka.js": 173,
+	"./kk.js": 174,
+	"./km.js": 175,
+	"./kn.js": 176,
+	"./ko.js": 177,
+	"./ku.js": 178,
+	"./ky.js": 179,
+	"./lb.js": 180,
+	"./lo.js": 181,
+	"./lt.js": 69,
+	"./lv.js": 70,
+	"./me.js": 182,
+	"./mi.js": 183,
+	"./mk.js": 184,
+	"./ml.js": 185,
+	"./mn.js": 186,
+	"./mr.js": 187,
+	"./ms-my.js": 188,
+	"./ms.js": 189,
+	"./mt.js": 71,
+	"./my.js": 190,
+	"./nb.js": 191,
+	"./ne.js": 192,
+	"./nl-be.js": 193,
+	"./nl.js": 62,
+	"./nn.js": 194,
+	"./pa-in.js": 195,
+	"./pl.js": 72,
+	"./pt-br.js": 196,
+	"./pt.js": 73,
+	"./ro.js": 74,
+	"./ru.js": 197,
+	"./sd.js": 198,
+	"./se.js": 199,
+	"./si.js": 200,
+	"./sk.js": 76,
+	"./sl.js": 75,
+	"./sq.js": 201,
+	"./sr-cyrl.js": 202,
+	"./sr.js": 203,
+	"./ss.js": 204,
+	"./sv.js": 77,
+	"./sw.js": 205,
+	"./ta.js": 206,
+	"./te.js": 207,
+	"./tet.js": 208,
+	"./tg.js": 209,
+	"./th.js": 210,
+	"./tl-ph.js": 211,
+	"./tlh.js": 212,
+	"./tr.js": 213,
+	"./tzl.js": 214,
+	"./tzm-latn.js": 215,
+	"./tzm.js": 216,
+	"./ug-cn.js": 217,
+	"./uk.js": 218,
+	"./ur.js": 219,
+	"./uz-latn.js": 220,
+	"./uz.js": 221,
+	"./vi.js": 222,
+	"./x-pseudo.js": 223,
+	"./yo.js": 224,
+	"./zh-cn.js": 225,
+	"./zh-hk.js": 226,
+	"./zh-tw.js": 227
+};
+
+
+function webpackContext(req) {
+	var id = webpackContextResolve(req);
+	return __webpack_require__(id);
+}
+function webpackContextResolve(req) {
+	if(!__webpack_require__.o(map, req)) {
+		var e = new Error("Cannot find module '" + req + "'");
+		e.code = 'MODULE_NOT_FOUND';
+		throw e;
+	}
+	return map[req];
+}
+webpackContext.keys = function webpackContextKeys() {
+	return Object.keys(map);
+};
+webpackContext.resolve = webpackContextResolve;
+module.exports = webpackContext;
+webpackContext.id = 20;
+
+/***/ })
+
+}]);
+//# sourceMappingURL=2-3611aa70791ee2e7501f.js.map
