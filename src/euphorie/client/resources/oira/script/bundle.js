6010270c814024fd4dc631ceb9e2908b930312ff
Alexander Pilz <pilz@syslab.com>
2020-04-13 20:53:45 +0200

    fix path

--- src/euphorie/client/resources/oira/script/bundle.js
+++ src/euphorie/client/resources/oira/script/bundle.js
@@ -183,7 +183,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "++resource++euphorie.resources/assets/oira/script/";
+/******/ 	__webpack_require__.p = "++resource++euphorie.resources/oira/script/";
 /******/
 /******/ 	// on error function for async loading
 /******/ 	__webpack_require__.oe = function(err) { console.error(err); throw err; };
