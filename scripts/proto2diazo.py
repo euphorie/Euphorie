9075f18c2f0c2e25cc081685b83e7b5afc211df3
Alexander Pilz <pilz@syslab.com>
2020-04-14 14:10:25 +0200

    url rewriter for help section

--- scripts/proto2diazo.py
+++ scripts/proto2diazo.py
@@ -8,6 +8,8 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(sys.argv[0])
 
 THEME_DIR = os.path.join("src", "euphorie", "client", "resources")
+HELP_DIR = os.path.join("src", "euphorie", "client",
+                        "resources", "oira", "help")
 
 
 def fix_urls(filepath):
@@ -22,17 +24,23 @@ def fix_urls(filepath):
         logger.exception("Problem reading %s", filepath)
         return
 
+    delta = len(filepath.split("/")) - 6
+    shim = "../" * delta
     content = content.replace("url(/media/", "url(++resource++euphorie.media/")
+    content = content.replace("=\"/assets/oira/", "=\"" + shim)
+    content = content.replace("=\"//assets/oira/", "=\"" + shim)
+    content = content.replace("=\"/media/", "=\"" + shim + "media/")
+
     open(filepath, "w").write(content)
 
 
 def run():
-    ## Recursively walk the theme directory and replace in all CSS files.
-    ## https://stackoverflow.com/a/3964691/1337474
-    # for root, dirs, files in os.walk(THEME_DIR):
-    #    for file in files:
-    #        if file.endswith(".css"):
-    #            fix_urls(os.path.join(root, file))
+    # Recursively walk the help directory and replace in all html files.
+    # https://stackoverflow.com/a/3964691/1337474
+    for root, dirs, files in os.walk(HELP_DIR):
+        for file in files:
+            if file.endswith(".html"):
+                fix_urls(os.path.join(root, file))
 
     fix_urls(os.path.join(THEME_DIR, "daimler", "style", "all.css"))
     fix_urls(os.path.join(THEME_DIR, "oira", "style", "all.css"))
