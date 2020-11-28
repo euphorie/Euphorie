fbbfba24dd6c1bd78afd041dd0e409cea290cec5
Wolfgang Thomas <thomas@syslab.com>
2020-11-28 19:04:47 +0100

    content: de-grok SectorContainer View

--- src/euphorie/content/sectorcontainer.py
+++ src/euphorie/content/sectorcontainer.py
@@ -6,18 +6,11 @@ A Sector Container provides the overview of all countries.
 
 https://admin.oiraproject.eu/sectors
 """
-from Acquisition import aq_inner
 from euphorie.content.behaviour.richdescription import IRichDescription
-from euphorie.content.utils import summarizeCountries
-from five import grok
 from plone.app.dexterity.behaviors.metadata import IBasic
 from plone.directives import dexterity
 from plone.directives import form
-from plonetheme.nuplone.skin.interfaces import NuPloneSkin
 from zope.interface import implements
-
-
-grok.templatedir("templates")
 
 
 class ISectorContainer(form.Schema, IRichDescription, IBasic):
@@ -30,14 +23,3 @@ class SectorContainer(dexterity.Container):
     def _canCopy(self, op=0):
         """Tell Zope2 that this object can not be copied."""
         return False
-
-
-class View(grok.View):
-    grok.context(ISectorContainer)
-    grok.require("zope2.View")
-    grok.layer(NuPloneSkin)
-    grok.template("sectorcontainer_view")
-    grok.name("nuplone-view")
-
-    def update(self):
-        self.countries = summarizeCountries(aq_inner(self.context), self.request)
