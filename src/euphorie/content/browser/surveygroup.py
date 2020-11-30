a9493842b920e98ee0ab03ab1fa333b74fce306c
Wolfgang Thomas <thomas@syslab.com>
2020-11-30 10:06:46 +0100

    content: de-grok SurveyGroup View and Add Form

---
+++ src/euphorie/content/browser/surveygroup.py
@@ -0,0 +1,192 @@
+# coding=utf-8
+from ..surveygroup import ISurveyGroup
+from Acquisition import aq_inner
+from Acquisition import aq_parent
+from euphorie.content import MessageFactory as _
+from OFS.event import ObjectClonedEvent
+from plone import api
+from plone.dexterity.browser.add import DefaultAddForm
+from plone.dexterity.browser.add import DefaultAddView
+from plone.dexterity.utils import createContentInContainer
+from Products.CMFCore.interfaces import ISiteRoot
+from Products.Five import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from ZODB.POSException import ConflictError
+from zope.component import getUtility
+from zope.event import notify
+
+import datetime
+
+
+class SurveyGroupView(BrowserView):
+    def __call__(self):
+        latest = aq_inner(self.context).values()[0]
+        self.request.response.redirect(latest.absolute_url())
+
+
+class AddForm(DefaultAddForm):
+    """Custom add form for :obj:`Survey` instances. This add form adds a
+    the :obj:`ITemplateSchema` schema, which allows users to pick a template
+    survey to use as a basis for the new survey.
+    """
+
+    portal_type = "euphorie.surveygroup"
+    schema = ISurveyGroup
+    template = ViewPageTemplateFile("templates/surveygroup_add.pt")
+
+    def update(self):
+        super(AddForm, self).update()
+        sector = aq_inner(self.context)
+        country = aq_parent(sector)
+        self.my_country = country.id
+        self.my_sector = sector.id
+
+    def buildSurveyTree(self):
+        site = getUtility(ISiteRoot)
+        catalog = api.portal.get_tool("portal_catalog")
+        startPath = "%s/sectors" % "/".join(site.getPhysicalPath())
+        startPathDepth = startPath.count("/") + 1
+        query = {
+            "path": {"query": startPath, "depth": 4, "navtree": True},
+            "portal_type": [
+                "euphorie.country",
+                "euphorie.sector",
+                "euphorie.surveygroup",
+                "euphorie.survey",
+            ],
+            "sort_on": "path",
+            "sort_order": "asc",
+        }
+        tree = {}
+        for brain in catalog.searchResults(query):
+            path = brain.getPath().split("/")[startPathDepth:]
+            if brain.portal_type == "euphorie.country":
+                tree[brain.id] = dict(
+                    id=brain.id, title=brain.Title, path=brain.getPath(), sectors={}
+                )
+            elif brain.portal_type == "euphorie.sector":
+                country = tree[path[0]]
+                country["sectors"][brain.id] = dict(
+                    id=brain.id, title=brain.Title, path=brain.getPath(), groups={}
+                )
+            elif brain.portal_type == "euphorie.surveygroup":
+                country = tree[path[0]]
+                sector = country["sectors"][path[1]]
+                sector["groups"][brain.id] = dict(
+                    id=brain.id, title=brain.Title, path=brain.getPath(), surveys={}
+                )
+            elif brain.portal_type == "euphorie.survey":
+                country = tree[path[0]]
+                sector = country["sectors"][path[1]]
+                group = sector["groups"][path[2]]
+                group["surveys"][brain.id] = dict(
+                    id=brain.id,
+                    title=brain.Title,
+                    path=brain.getPath(),
+                    url=brain.getURL(),
+                )
+        my_sector_path = tree[self.my_country]["sectors"][self.my_sector]["path"]
+        countries = sorted(tree.values(), key=lambda x: x["title"])
+        self.my_group = None
+        for country in countries:
+            country["sectors"] = sectors = sorted(
+                country["sectors"].values(), key=lambda x: x["title"]
+            )
+
+            for sector in sectors:
+                sector["groups"] = groups = sorted(
+                    sector["groups"].values(), key=lambda x: x["title"]
+                )
+
+                if sector["path"] == my_sector_path and sector["groups"]:
+                    self.my_group = sector["groups"][0]["id"]
+                for group in groups:
+                    group["surveys"] = sorted(
+                        group["surveys"].values(), key=lambda x: x["title"]
+                    )
+
+                sector["groups"] = [g for g in groups if g["surveys"]]
+            country["sectors"] = [s for s in sectors if s["groups"]]
+        countries = [c for c in countries if c["sectors"]]
+        return countries
+
+    def copyTemplate(self, source, target):
+        target = self.context[target.id]  # Acquisition-wrap
+        try:
+            source._notifyOfCopyTo(target, op=0)
+        except ConflictError:
+            raise
+
+        copy = source._getCopy(target)
+
+        today = datetime.date.today()
+        title = self.request.locale.dates.getFormatter("date", length="long").format(
+            datetime.date.today()
+        )
+        copy.id = today.isoformat()
+        copy.title = title
+        source_algorithm = aq_parent(source).evaluation_algorithm
+        target_algorithm = self.request.form.get(
+            "form.widgets.evaluation_algorithm", [source_algorithm]
+        ).pop()
+        target.evaluation_algorithm = target_algorithm
+        target._setObject(copy.id, copy)
+        if source_algorithm != target_algorithm:
+            from euphorie.content.risk import IRisk
+            from euphorie.content.risk import EnsureInterface
+
+            risks = [
+                item
+                for (id, item) in target.ZopeFind(target, search_sub=1)
+                if IRisk.providedBy(item)
+            ]
+            for risk in risks:
+                EnsureInterface(risk)
+
+        if hasattr(copy, "published"):
+            delattr(copy, "published")
+        copy = target[copy.id]  # Acquisition-wrap
+        copy.wl_clearLocks()
+        copy._postCopy(target, op=0)
+
+        wt = api.portal.get_tool("portal_workflow")
+        if wt.getInfoFor(copy, "review_state") == "published":
+            wt.doActionFor(copy, "retract")
+
+        notify(ObjectClonedEvent(target[copy.id]))
+        return copy
+
+    def createAndAdd(self, data):
+        obj = super(AddForm, self).createAndAdd(data)
+        obj = aq_inner(self.context)[obj.id]
+
+        form = self.request.form
+        if form["source"] != "scratch":
+            sector = aq_inner(self.context)
+            if form["source"] == "other":
+                country_id = form["country"]
+                country = aq_parent(aq_parent(sector))[country_id]
+                bits = form["sector.%s" % country_id].split(".", 1)
+                sector = country[bits[0]]
+                surveygroup = sector[bits[1]]
+                survey_id = form["survey.%s.%s" % (country_id, surveygroup.id)]
+                survey = surveygroup[survey_id]
+            else:
+                sg_local = form["surveygroup.local"]
+                surveygroup = sector[sg_local]
+                survey = surveygroup[form["survey.local.%s" % sg_local]]
+
+            survey = self.copyTemplate(survey, obj)
+            self.immediate_view = survey.absolute_url()
+        else:
+            title = api.portal.translate(_("survey_default_title", default=u"Standard"))
+
+            obj = aq_inner(self.context)[obj.id]
+            survey = createContentInContainer(obj, "euphorie.survey", title=title)
+
+            self.immediate_view = survey.absolute_url()
+        return obj
+
+
+class AddView(DefaultAddView):
+    form = AddForm
--- src/euphorie/content/surveygroup.py
+++ src/euphorie/content/surveygroup.py
@@ -13,29 +13,21 @@ from Acquisition import aq_parent
 from euphorie.content.interfaces import SurveyUnpublishEvent
 from euphorie.content.survey import ISurvey
 from five import grok
-from OFS.event import ObjectClonedEvent
 from plone.app.dexterity.behaviors.metadata import IBasic
-from plone.dexterity.utils import createContentInContainer
 from plone.directives import dexterity
 from plone.directives import form
 from plonetheme.nuplone.skin.interfaces import NuPloneSkin
 from Products.CMFCore.interfaces import IActionSucceededEvent
-from Products.CMFCore.interfaces import ISiteRoot
 from Products.CMFCore.utils import getToolByName
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.statusmessages.interfaces import IStatusMessage
 from z3c.form.interfaces import IEditForm
-from ZODB.POSException import ConflictError
 from zope import schema
-from zope.component import getUtility
 from zope.event import notify
-from zope.i18n import translate
 from zope.interface import implements
 from zope.lifecycleevent.interfaces import IObjectRemovedEvent
 from zope.schema.vocabulary import SimpleTerm
 from zope.schema.vocabulary import SimpleVocabulary
 
-import datetime
 import logging
 import urllib
 
@@ -100,185 +92,6 @@ class SurveyGroup(dexterity.Container):
     def _canCopy(self, op=0):
         """Tell Zope2 that this object can not be copied."""
         return op
-
-
-class View(grok.View):
-    grok.context(ISurveyGroup)
-    grok.require("zope2.View")
-    grok.layer(NuPloneSkin)
-    grok.name("nuplone-view")
-
-    def render(self):
-        latest = aq_inner(self.context).values()[0]
-        self.request.response.redirect(latest.absolute_url())
-
-
-class AddForm(dexterity.AddForm):
-    """Custom add form for :obj:`Survey` instances. This add form adds a
-    the :obj:`ITemplateSchema` schema, which allows users to pick a template
-    survey to use as a basis for the new survey.
-    """
-
-    grok.context(ISurveyGroup)
-    grok.name("euphorie.surveygroup")
-    grok.require("euphorie.content.AddNewRIEContent")
-
-    template = ViewPageTemplateFile("templates/surveygroup_add.pt")
-
-    def update(self):
-        super(AddForm, self).update()
-        sector = aq_inner(self.context)
-        country = aq_parent(sector)
-        self.my_country = country.id
-        self.my_sector = sector.id
-
-    def buildSurveyTree(self):
-        site = getUtility(ISiteRoot)
-        catalog = getToolByName(self.context, "portal_catalog")
-        startPath = "%s/sectors" % "/".join(site.getPhysicalPath())
-        startPathDepth = startPath.count("/") + 1
-        query = {
-            "path": {"query": startPath, "depth": 4, "navtree": True},
-            "portal_type": [
-                "euphorie.country",
-                "euphorie.sector",
-                "euphorie.surveygroup",
-                "euphorie.survey",
-            ],
-            "sort_on": "path",
-            "sort_order": "asc",
-        }
-        tree = {}
-        for brain in catalog.searchResults(query):
-            path = brain.getPath().split("/")[startPathDepth:]
-            if brain.portal_type == "euphorie.country":
-                tree[brain.id] = dict(
-                    id=brain.id, title=brain.Title, path=brain.getPath(), sectors={}
-                )
-            elif brain.portal_type == "euphorie.sector":
-                country = tree[path[0]]
-                country["sectors"][brain.id] = dict(
-                    id=brain.id, title=brain.Title, path=brain.getPath(), groups={}
-                )
-            elif brain.portal_type == "euphorie.surveygroup":
-                country = tree[path[0]]
-                sector = country["sectors"][path[1]]
-                sector["groups"][brain.id] = dict(
-                    id=brain.id, title=brain.Title, path=brain.getPath(), surveys={}
-                )
-            elif brain.portal_type == "euphorie.survey":
-                country = tree[path[0]]
-                sector = country["sectors"][path[1]]
-                group = sector["groups"][path[2]]
-                group["surveys"][brain.id] = dict(
-                    id=brain.id,
-                    title=brain.Title,
-                    path=brain.getPath(),
-                    url=brain.getURL(),
-                )
-        my_sector_path = tree[self.my_country]["sectors"][self.my_sector]["path"]
-        countries = sorted(tree.values(), key=lambda x: x["title"])
-        self.my_group = None
-        for country in countries:
-            country["sectors"] = sectors = sorted(
-                country["sectors"].values(), key=lambda x: x["title"]
-            )
-
-            for sector in sectors:
-                sector["groups"] = groups = sorted(
-                    sector["groups"].values(), key=lambda x: x["title"]
-                )
-
-                if sector["path"] == my_sector_path and sector["groups"]:
-                    self.my_group = sector["groups"][0]["id"]
-                for group in groups:
-                    group["surveys"] = sorted(
-                        group["surveys"].values(), key=lambda x: x["title"]
-                    )
-
-                sector["groups"] = [g for g in groups if g["surveys"]]
-            country["sectors"] = [s for s in sectors if s["groups"]]
-        countries = [c for c in countries if c["sectors"]]
-        return countries
-
-    def copyTemplate(self, source, target):
-        target = self.context[target.id]  # Acquisition-wrap
-        try:
-            source._notifyOfCopyTo(target, op=0)
-        except ConflictError:
-            raise
-
-        copy = source._getCopy(target)
-
-        today = datetime.date.today()
-        title = self.request.locale.dates.getFormatter("date", length="long").format(
-            datetime.date.today()
-        )
-        copy.id = today.isoformat()
-        copy.title = title
-        source_algorithm = aq_parent(source).evaluation_algorithm
-        target_algorithm = self.request.form.get(
-            "form.widgets.evaluation_algorithm", [source_algorithm]
-        ).pop()
-        target.evaluation_algorithm = target_algorithm
-        target._setObject(copy.id, copy)
-        if source_algorithm != target_algorithm:
-            from euphorie.content.risk import IRisk
-            from euphorie.content.risk import EnsureInterface
-
-            risks = [
-                item
-                for (id, item) in target.ZopeFind(target, search_sub=1)
-                if IRisk.providedBy(item)
-            ]
-            for risk in risks:
-                EnsureInterface(risk)
-
-        if hasattr(copy, "published"):
-            delattr(copy, "published")
-        copy = target[copy.id]  # Acquisition-wrap
-        copy.wl_clearLocks()
-        copy._postCopy(target, op=0)
-
-        wt = getToolByName(copy, "portal_workflow")
-        if wt.getInfoFor(copy, "review_state") == "published":
-            wt.doActionFor(copy, "retract")
-
-        notify(ObjectClonedEvent(target[copy.id]))
-        return copy
-
-    def createAndAdd(self, data):
-        obj = super(AddForm, self).createAndAdd(data)
-        obj = aq_inner(self.context)[obj.id]
-
-        form = self.request.form
-        if form["source"] != "scratch":
-            sector = aq_inner(self.context)
-            if form["source"] == "other":
-                country_id = form["country"]
-                country = aq_parent(aq_parent(sector))[country_id]
-                bits = form["sector.%s" % country_id].split(".", 1)
-                sector = country[bits[0]]
-                surveygroup = sector[bits[1]]
-                survey_id = form["survey.%s.%s" % (country_id, surveygroup.id)]
-                survey = surveygroup[survey_id]
-            else:
-                sg_local = form["surveygroup.local"]
-                surveygroup = sector[sg_local]
-                survey = surveygroup[form["survey.local.%s" % sg_local]]
-
-            survey = self.copyTemplate(survey, obj)
-            self.immediate_view = survey.absolute_url()
-        else:
-            title = translate(
-                _("survey_default_title", default=u"Standard"), context=self.request
-            )
-
-            obj = aq_inner(self.context)[obj.id]
-            survey = createContentInContainer(obj, "euphorie.survey", title=title)
-
-            self.immediate_view = survey.absolute_url()
-        return obj
 
 
 class Unpublish(grok.View):
