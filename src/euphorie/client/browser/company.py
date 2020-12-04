32e657ce1f35e109094d1b9ac268f3764a36c6ea
ale-rt <alessandro.pisa@gmail.com>
2020-12-04 12:16:42 +0100

    Degrok @@about and @@report_company

--- src/euphorie/client/browser/company.py
+++ src/euphorie/client/browser/company.py
@@ -1,27 +1,15 @@
-"""
-Company
--------
-
-View and update the company survey.
-"""
-
-from .. import MessageFactory as _
+from euphorie import MessageFactory as _
 from euphorie.client import model
-from euphorie.client.adapters.session_traversal import ITraversedSurveySession
-from euphorie.client.interfaces import IClientSkinLayer
-from five import grok
 from plone import api
 from plone.directives import form
 from plone.memoize.view import memoize
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from z3c.form import button
 from z3c.form.form import applyChanges
 from zope import schema
 from zope.interface import directlyProvides
 from zope.schema.vocabulary import SimpleTerm
 from zope.schema.vocabulary import SimpleVocabulary
-
-
-grok.templatedir("templates")
 
 
 class CompanySchema(form.Schema):
@@ -124,15 +112,12 @@ class Company(form.SchemaForm):
 
     View name: @@report_company
     """
-    grok.context(ITraversedSurveySession)
-    grok.require("euphorie.client.ViewSurvey")
-    grok.layer(IClientSkinLayer)
-    grok.template("report_company")
-    grok.name("report_company")
+
     variation_class = "variation-risk-assessment"
 
     schema = CompanySchema
     company = None
+    template = ViewPageTemplateFile("templates/report_company.pt")
 
     @property
     def session(self):
@@ -148,9 +133,10 @@ class Company(form.SchemaForm):
         self.company = session.company
 
     def countries(self):
-        names = [{'id': key.lower(), 'title': value}
-                 for (key, value) in
-                 self.request.locale.displayNames.territories.items()]
+        names = [
+            {"id": key.lower(), "title": value}
+            for (key, value) in self.request.locale.displayNames.territories.items()
+        ]
         names.sort(key=lambda c: c["title"])
         return names
 
@@ -198,13 +184,13 @@ class Company(form.SchemaForm):
         # XXX: This a hack. We need to know if a company report has been
         # skipped but can't add new SQL columns. So we mark the country 'xx'.
         data = {
-            'conductor': None,
-            'country': u'xx',
-            'employees': None,
-            'referer': None,
-            'workers_participated': None,
-            'needs_met': None,
-            'recommend_tool': None,
+            "conductor": None,
+            "country": u"xx",
+            "employees": None,
+            "referer": None,
+            "workers_participated": None,
+            "needs_met": None,
+            "recommend_tool": None,
         }
         self.applyChanges(data)
         url = "%s/@@report_view" % self.context.absolute_url()
