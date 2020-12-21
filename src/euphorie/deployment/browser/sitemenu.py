d95c530d7ce101af554a2499bd8a92ff52807afd
Wolfgang Thomas <thomas@syslab.com>
2020-12-21 15:56:08 +0100

    In NuPlone, sitemenu got de-grokked. That means our customisation needs to be registered in zcml

--- src/euphorie/deployment/browser/sitemenu.py
+++ src/euphorie/deployment/browser/sitemenu.py
@@ -1,17 +1,18 @@
-from Acquisition import aq_inner
+# coding=utf-8
 from AccessControl import getSecurityManager
-from Products.CMFCore.utils import getToolByName
-from Products.membrane.interfaces.user import IMembraneUser
-from plonetheme.nuplone.utils import getPortal
-from plonetheme.nuplone.utils import checkPermission
-from plonetheme.nuplone.skin.sitemenu import Sitemenu
-from plonetheme.nuplone import MessageFactory as nu_
+from Acquisition import aq_inner
 from euphorie.content import MessageFactory as _
 from euphorie.content.sector import ISector
 from euphorie.content.survey import ISurvey
+from plonetheme.nuplone import MessageFactory as nu_
+from plonetheme.nuplone.skin import sitemenu
+from plonetheme.nuplone.utils import checkPermission
+from plonetheme.nuplone.utils import getPortal
+from Products.CMFCore.utils import getToolByName
+from Products.membrane.interfaces.user import IMembraneUser
 
 
-class EuphorieSitemenu(Sitemenu):
+class Sitemenu(sitemenu.Sitemenu):
     @property
     def settings_url(self):
         user = getSecurityManager().getUser()
@@ -24,7 +25,7 @@ class EuphorieSitemenu(Sitemenu):
             return "%s/@@settings" % home.absolute_url()
 
     def organise(self):
-        menu = super(EuphorieSitemenu, self).organise()
+        menu = super(Sitemenu, self).organise()
         if menu is not None:
             children = menu["children"]
         else:
@@ -32,15 +33,22 @@ class EuphorieSitemenu(Sitemenu):
             children = menu["children"] = []
 
         context_url = aq_inner(self.context).absolute_url()
-        if ISurvey.providedBy(self.context) and \
-                checkPermission(self.context, "View"):
-            children.append({"title": _("menu_export", default=u"XML export"),
-                             "url": "%s/@@export" % context_url})
-        if ISector.providedBy(self.context) and \
-                checkPermission(self.context, "Euphorie: Add new RIE Content"):
+        if ISurvey.providedBy(self.context) and checkPermission(self.context, "View"):
             children.append(
-                    {"title": _("menu_import", default=u"Import OiRA Tool"),
-                     "url": "%s/@@upload" % context_url})
+                {
+                    "title": _("menu_export", default=u"XML export"),
+                    "url": "%s/@@export" % context_url,
+                }
+            )
+        if ISector.providedBy(self.context) and checkPermission(
+            self.context, "Euphorie: Add new RIE Content"
+        ):
+            children.append(
+                {
+                    "title": _("menu_import", default=u"Import OiRA Tool"),
+                    "url": "%s/@@upload" % context_url,
+                }
+            )
         if children:
             return menu
         else:
