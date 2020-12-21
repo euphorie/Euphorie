a65968bdcc1cd0457332803e9b9e9dc6df80e244
Wolfgang Thomas <thomas@syslab.com>
2020-12-21 12:33:00 +0100

    Re-import the SurveyGroup type definition to fix a strange error in some existing installations of Euphorie. Fixes #273

---
+++ src/euphorie/upgrade/content/v1/20201219143658_reimport_surveygroup/upgrade.py
@@ -0,0 +1,14 @@
+from ftw.upgrade import UpgradeStep
+from plone import api
+
+
+class ReImportSurveyGroup(UpgradeStep):
+    """Re-import SurveyGroup type definition"""
+
+    def __call__(self):
+        # First, delete exisiting type registration for euphorie.surveygroup
+        tt = api.portal.get_tool("portal_types")
+        obj = tt.get("euphorie.surveygroup")
+        if obj:
+            api.content.delete(obj)
+        self.install_upgrade_profile()
