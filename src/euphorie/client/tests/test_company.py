a8a63e4b2c0cb9961bce5438c6ce326fe170e746
Manuel Reinhardt <reinhardt@syslab.com>
2021-04-09 13:29:04 +0200

    Added timestamp field to company. Slc refs SCR-953

---
+++ src/euphorie/client/tests/test_company.py
@@ -0,0 +1,59 @@
+# coding=utf-8
+from datetime import datetime
+from datetime import timedelta
+from euphorie.client import model
+from euphorie.client.company import Company
+from euphorie.client.tests.utils import addAccount
+from euphorie.client.tests.utils import addSurvey
+from euphorie.content.tests.utils import BASIC_SURVEY
+from euphorie.testing import EuphorieIntegrationTestCase
+from plone import api
+import mock
+
+
+class TestCompanyViews(EuphorieIntegrationTestCase):
+    """"""
+
+    @mock.patch.object(Company, "applyChanges")
+    @mock.patch.object(Company, "render")
+    def test_company_timestamp(self, mock_render, mock_applyChanges):
+        survey = addSurvey(self.portal, BASIC_SURVEY)
+        account = addAccount(password="secret")
+        survey_session = model.SurveySession(
+            id=456,
+            title=u"Dummy session",
+            created=datetime(2021, 4, 9, 9, 11, 31),
+            modified=datetime(2021, 4, 9, 9, 11, 52),
+            zodb_path="nl/ict/software-development",
+            account=account,
+        )
+        model.Session.add(survey_session)
+        survey = self.portal.client.nl.ict["software-development"]
+
+        session_id = "++session++%d" % survey_session.id
+        traversed_survey_session = survey.restrictedTraverse(session_id)
+
+        with api.env.adopt_user(user=survey_session.account):
+            with self._get_view(
+                "report_company", traversed_survey_session, survey_session
+            ) as view:
+                view.request.form = {
+                    "form.widgets.conductor": u"staff",
+                    "form.widgets.country": u"nl",
+                    "form.widgets.employees": u"10-49",
+                    "form.widgets.referer": u"health-safety-experts",
+                    "form.widgets.workers_participated": False,
+                    "form.widgets.needs_met": True,
+                    "form.widgets.recommend_tool": True,
+                    "form.buttons.next": "",
+                }
+                view()
+                timestamp = mock_applyChanges.call_args[0][0]["timestamp"]
+                self.assertLess(datetime.now() - timestamp, timedelta(seconds=3))
+
+                del view.request.form["form.buttons.next"]
+                view.request.form["form.buttons.previous"] = ""
+                view()
+                self.assertEqual(mock_applyChanges.call_count, 2)
+                timestamp = mock_applyChanges.call_args[0][0]["timestamp"]
+                self.assertLess(datetime.now() - timestamp, timedelta(seconds=3))
