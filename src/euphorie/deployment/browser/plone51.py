707ebcc54442485a80aa15f5a185b30e9c7bc8ea
Wolfgang Thomas <thomas@syslab.com>
2021-07-28 17:58:24 +0200

    Add view for removing broken behaviours

--- src/euphorie/deployment/browser/plone51.py
+++ src/euphorie/deployment/browser/plone51.py
@@ -1,7 +1,8 @@
 # coding=utf-8
-# coding=utf-8
 from plone import api
 from plone.app.upgrade.utils import loadMigrationProfile
+from plone.behavior.registration import lookup_behavior_registration
+from plone.dexterity.interfaces import IDexterityFTI
 from Products.Five import BrowserView
 
 import logging
@@ -108,3 +109,20 @@ class UnistallArchetypes(BrowserView):
 
         # reapply uninstall to get rid of IATCTTool component
         loadMigrationProfile(portal, "profile-Products.ATContentTypes:uninstall")
+
+
+class RemoveBrokenBehaviors(BrowserView):
+    def __call__(self):
+        ptt = api.portal.get_tool("portal_types")
+        ftis = [fti for fti in ptt.objectValues() if IDexterityFTI.providedBy(fti)]
+        for fti in ftis:
+            bad_behaviors = []
+            for behavior in fti.behaviors:
+                try:
+                    lookup_behavior_registration(behavior)
+                except Exception:
+                    bad_behaviors.append(behavior)
+            if bad_behaviors:
+                behaviors = [x for x in fti.behaviors if x not in bad_behaviors]
+                fti.behaviors = behaviors
+                log.warning("Removed {} for FTI {}".format(bad_behaviors, fti.id))
