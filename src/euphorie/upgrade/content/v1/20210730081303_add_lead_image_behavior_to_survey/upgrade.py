990290927bb45f584cebfa4be1edf8a4952b0224
Manuel Reinhardt <reinhardt@syslab.com>
2021-07-30 08:50:33 +0200

    Lead image for survey. Refs SCR-1280

---
+++ src/euphorie/upgrade/content/v1/20210730081303_add_lead_image_behavior_to_survey/upgrade.py
@@ -0,0 +1,9 @@
+from ftw.upgrade import UpgradeStep
+
+
+class AddLeadImageBehaviorToSurvey(UpgradeStep):
+    """Add lead image behavior to survey.
+    """
+
+    def __call__(self):
+        self.install_upgrade_profile()
