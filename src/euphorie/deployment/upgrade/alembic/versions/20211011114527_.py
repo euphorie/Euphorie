eca6189a35ff919027a4c14fd4c02bcb76005bed
ale-rt <alessandro.pisa@gmail.com>
2021-10-11 12:00:36 +0200

    Replace the active column with a deactivated one
    Instead of having an active boolean flag,
    the groups have now a deactivated datetime column.

---
+++ src/euphorie/deployment/upgrade/alembic/versions/20211011114527_.py
@@ -0,0 +1,33 @@
+"""empty message
+
+Revision ID: 20211011114527
+Revises: 20210409113814
+Create Date: 2021-10-11 09:45:27.876273
+
+"""
+from alembic import op
+
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "20211011114527"
+down_revision = "20210409113814"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    """The boolean active column becomes the deactivated date time column"""
+    op.add_column("group", sa.Column("deactivated", sa.DateTime(), nullable=True))
+    op.execute("""UPDATE "group" SET deactivated = '1970-01-01' WHERE active = FALSE""")
+    op.drop_column("group", "active")
+
+
+def downgrade():
+    op.add_column(
+        "group", sa.Column("active", sa.BOOLEAN(), autoincrement=False, nullable=True)
+    )
+    op.execute("""UPDATE "group" SET active = TRUE WHERE deactivated IS NULL""")
+    op.execute("""UPDATE "group" SET active = FALSE WHERE deactivated IS NOT NULL""")
+    op.drop_column("group", "deactivated")
