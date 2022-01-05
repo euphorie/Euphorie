837e0979eb81e060992fbaf78073aa70c0590510
Wolfgang Thomas <thomas@syslab.com>
2022-01-05 15:46:51 +0100

    pyrtf-ng is actually no longer used Refs #254

--- setup.py
+++ setup.py
@@ -72,7 +72,6 @@ setup(
         "zope.schema",
         "NuPlone >=2.0.0",
         "plone.uuid",
-        "pyrtf-ng",
         "openpyxl",
         "Chameleon",
         "backports.tempfile;python_version<'3'",
