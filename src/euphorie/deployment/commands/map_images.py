201ad465bacde6e50181317639df0352c2655254
Manuel Reinhardt <reinhardt@syslab.com>
2022-07-05 09:16:37 +0200

    map_images: Refactored. * Bail out early if tool already has an image. * Improved exception handling. * Readability. Refs syslabcom/scrum#349

--- src/euphorie/deployment/commands/map_images.py
+++ src/euphorie/deployment/commands/map_images.py
@@ -1,7 +1,6 @@
 from lxml import etree
 from pathlib import posixpath
 from plone.namedfile.file import NamedBlobImage
-from tempfile import TemporaryDirectory
 from urllib.parse import unquote
 from urllib.parse import urlparse
 from zope.component.hooks import setSite
@@ -26,6 +25,18 @@ else:
 app = locals()["app"]
 setSite(app["Plone2"])
 wt = app["Plone2"]["portal_workflow"]
+
+
+def get_filename():
+    sourcename = urlparse(img.attrib["src"]).path.split("/")[-1]
+    basename = unquote(sourcename.split(".")[0]).strip()
+    if " " in basename:
+        name = " ".join([part for part in basename.split(" ")[:-1]])
+    else:
+        name = basename
+    filename = "{} 300.png".format(name)
+    return filename
+
 
 for page_num in range(255):
     url = "{}/en/oira-tools?search_api_fulltext=&sort_by=title" "&page={}".format(
@@ -54,52 +65,43 @@ for page_num in range(255):
         except KeyError:
             log.warning("Tool not found: {}".format(path))
             continue
+        if all(getattr(survey, "image", None) for survey in surveygroup.values()):
+            log.warning("Already has image: {}".format(path))
+            continue
 
         img = elem.find(".//div[@class='views-field views-field-field-image']//img")
         if img is None:
             log.warning("No image for {}".format(path))
             continue
-        sourcename = urlparse(img.attrib["src"]).path.split("/")[-1]
-        basename = unquote(sourcename.split(".")[0]).strip()
-        if " " in basename:
-            name = " ".join([part for part in basename.split(" ")[:-1]])
-        else:
-            name = basename
-        filename = "{} 300.png".format(name)
+        filename = get_filename()
         filepath = posixpath.join(images_path, filename)
         blob_image = None
         if not posixpath.exists(filepath):
             log.warning(
                 "{}: Image file not found: {} ({}). Attempting download".format(
-                    path, filepath, sourcename
+                    path, filepath, img.attrib["src"]
                 )
             )
-            with TemporaryDirectory(prefix="euphorieimage") as tmpdir:
+            try:
                 urllib.request.urlretrieve(
                     "{}{}".format(BASE_URL, img.attrib["src"]), filepath
                 )
-                try:
-                    with open(filepath, "rb") as imagefile:
-                        blob_image = NamedBlobImage(
-                            data=imagefile.read(), filename=filename
-                        )
-                except Exception as e:
-                    log.warning(
-                        "Unable to download image from website. Error: {}".format(e)
-                    )
-                    continue
-        else:
+            except Exception as e:
+                log.warning(
+                    "Unable to download image from website. Error: {}".format(e)
+                )
+                continue
+        try:
             with open(filepath, "rb") as imagefile:
                 blob_image = NamedBlobImage(data=imagefile.read(), filename=filename)
+        except Exception as e:
+            log.warning("Unable to open image. Error: {}".format(e))
+            continue
 
         if not blob_image:
             continue
         for survey in surveygroup.values():
-            if getattr(survey, "image", None):
-                # log.warning("Already has image: {}".format(path))
-                continue
-            else:
-                setattr(survey, "image", blob_image)
+            setattr(survey, "image", blob_image)
 
             if getattr(surveygroup, "published", None) == survey.getId():
                 try:
