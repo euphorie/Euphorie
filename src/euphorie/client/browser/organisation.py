6ac7be8dbc5cc3539e1dc435951cb4571b18da29
Manuel Reinhardt <reinhardt@syslab.com>
2023-04-18 09:22:05 +0200

    Ask for confirmation when following an organisation invite link (#529)
    Ask for confirmation when following an organisation invite link
    
    Co-authored-by: Alessandro Pisa <alessandro.pisa@gmail.com>
    syslabcom/scrum#594

--- src/euphorie/client/browser/organisation.py
+++ src/euphorie/client/browser/organisation.py
@@ -25,6 +25,11 @@ from zope.interface import alsoProvides
 from zope.interface import implementer
 from zope.publisher.interfaces import IPublishTraverse
 
+import logging
+
+
+logger = logging.getLogger(__name__)
+
 
 class OrganisationBaseView(BaseView):
     """Base class for all organisation views."""
@@ -337,6 +342,27 @@ class ConfirmInvite(BaseView):
         )
 
     @property
+    @memoize_contextless
+    def inviter(self):
+        if not self.token_value:
+            return None
+        userid = self.token_value["userid"]
+        try:
+            user = self.sqlsession.query(Account).filter(Account.id == userid).first()
+        except Exception:
+            logger.warning("Unable to fetch account for username:")
+            logger.warning(userid)
+            return None
+        return user
+
+    @property
+    def inviter_fullname(self):
+        user = self.inviter
+        if user is None:
+            return ""
+        return user.title
+
+    @property
     def default_target_view(self):
         view = "@@organisation"
         organisation = self.organisation
@@ -435,8 +461,21 @@ class ConfirmInvite(BaseView):
             )
         )
 
-    def __call__(self):
-        self.lookup_token_and_redirect()
+    def handle_POST(self):
+        if self.request.form.get("submit", "") == "accept":
+            self.lookup_token_and_redirect()
+        else:
+            return self.redirect(
+                msg=_(
+                    "message_add_user_to_organisation_declined",
+                    default=(
+                        "You have declined the invitation to the ${name} "
+                        "organisation."
+                    ),
+                    mapping={"name": self.organisation.title},
+                ),
+                target=f"{self.context.absolute_url()}",
+            )
 
 
 @implementer(IPublishTraverse)
@@ -542,26 +581,7 @@ class OrganisationLogo(OrganisationBaseView):
         if not organisation:
             return
 
-        # Check if the organisation should be visible to the current account
-        account_id = account.id
-        if organisation.owner_id == account_id:
-            # The user is the owner, return the organisation
-            return organisation
-
-        if (
-            self.sqlsession.query(OrganisationMembership)
-            .filter(
-                sql.and_(
-                    OrganisationMembership.owner_id == organisation.owner_id,
-                    OrganisationMembership.member_id == account_id,
-                )
-            )
-            .count()
-        ):
-            # The user is a member, return the organisation
-            return organisation
-
-        raise Unauthorized("You are not a member of this organisation")
+        return organisation
 
     def get_or_create_image_scaled(self):
         """Get the image scaled."""
