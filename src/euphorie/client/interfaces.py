900c6c7dfcab5f67fd6a0016f058bdccf13eeb7e
Johannes Raggam <thetetet@gmail.com>
2023-10-19 10:33:43 +0200

    scrum-1572: Define a notification category adapter.

--- src/euphorie/client/interfaces.py
+++ src/euphorie/client/interfaces.py
@@ -1,4 +1,5 @@
 from plonetheme.nuplone.z3cform.interfaces import INuPloneFormLayer
+from zope.interface import Attribute
 from zope.interface import implementer
 from zope.interface import Interface
 from zope.interface.interfaces import ObjectEvent
@@ -16,3 +17,14 @@ class ICustomRisksModifiedEvent(Interface):
 @implementer(ICustomRisksModifiedEvent)
 class CustomRisksModifiedEvent(ObjectEvent):
     """Custom risks were modified."""
+
+
+class INotificationCategory(Interface):
+    """A notification category adapter."""
+
+    id = Attribute("Id")  # The id of the category
+    title = Attribute("Title")  # The title of the category
+    description = Attribute("Description")  # A description of the category
+
+    def notify():
+        """Send a notification."""
