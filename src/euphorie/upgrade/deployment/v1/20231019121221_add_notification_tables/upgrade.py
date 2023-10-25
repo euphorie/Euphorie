b6ffcf76c735b636aaa274ae6569a8ff82afdacc
Johannes Raggam <thetetet@gmail.com>
2023-10-25 03:57:56 +0200

    Notifications: Add euphorie.notification__ra_not_modified__reminder_days registry record.

--- src/euphorie/upgrade/deployment/v1/20231019121221_add_notification_tables/upgrade.py
+++ src/euphorie/upgrade/deployment/v1/20231019121221_add_notification_tables/upgrade.py
@@ -6,4 +6,5 @@ class AddNotificationTables(UpgradeStep):
     """Add notification tables."""
 
     def __call__(self):
+        self.install_upgrade_profile()
         alembic_upgrade_to(self.target_version)
