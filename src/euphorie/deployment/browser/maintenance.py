a91830cc1f9c731eb1eba090506ed4a1feeff6ac
ale-rt <alessandro.pisa@gmail.com>
2025-03-27 15:57:39 +0100

    Add the admin-maintenance view
    The view uses the actions defined in the newly introduced maintenance_actions category to be customizable per project.
    Some default actions are expected to land in the code after a first test phase.
    
    Refs. https://github.com/syslabcom/scrum/issues/3197

--- src/euphorie/deployment/browser/maintenance.py
+++ src/euphorie/deployment/browser/maintenance.py
@@ -1,6 +1,9 @@
 from euphorie.client.model import Account
 from euphorie.client.model import Session
 from euphorie.client.model import SurveySession
+from plone import api
+from Products.CMFCore.ActionInformation import ActionInfo
+from Products.CMFPlone.ActionsTool import ActionsTool
 from Products.Five import BrowserView
 from transaction import commit
 
@@ -38,3 +41,15 @@ class CleanUpLastModifierId(BrowserView):
                     logger.info("Deleted user %s", guest_user.id)
         commit()
         return "Done"
+
+
+class AdminMaintenanceView(BrowserView):
+    """View for maintenance tasks (@@admin-maintenance)."""
+
+    @property
+    def actions(self) -> tuple[ActionInfo]:
+        """Return all the actions available in the `maintenance_actions` category."""
+        portal_actions: ActionsTool = api.portal.get_tool("portal_actions")
+        return portal_actions.listActionInfos(
+            object=self.context, categories=["maintenance_actions"]
+        )
