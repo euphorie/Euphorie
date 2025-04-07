eb1d388bfaf90a2ac0a03528043f1bc5c424f0aa
Manuel Reinhardt <reinhardt@syslab.com>
2025-04-07 15:25:15 +0200

    Choice: add field allow_multiple_options

--- src/euphorie/content/choice.py
+++ src/euphorie/content/choice.py
@@ -1,7 +1,21 @@
+from .. import MessageFactory as _
 from .behaviour.richdescription import IRichDescription
 from plone.app.dexterity.behaviors.metadata import IBasic
 from plone.supermodel import model
+from zope import schema
 
 
 class IChoice(model.Schema, IRichDescription, IBasic):
     """ """
+
+    allow_multiple_options = schema.Bool(
+        title=_(
+            "label_allow_multiple_options", default="Allow multiple options"
+        ),
+        description=_(
+            "help_allow_multiple_options",
+            default="If active, checkboxes are shown to allow selecting more than one "
+            "option. Otherwise, radio buttons are used to force a single selection.",
+        ),
+        default=True,
+    )
