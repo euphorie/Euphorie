f1c3850d1ea9686391145331e9d99a19f7cf76c5
Manuel Reinhardt <reinhardt@syslab.com>
2025-05-06 16:54:49 +0200

    Add tool type `inventory` and allow Choice only in this type

--- src/euphorie/content/choice.py
+++ src/euphorie/content/choice.py
@@ -1,5 +1,10 @@
 from .. import MessageFactory as _
 from .behaviour.richdescription import IRichDescription
+from .fti import ConditionalDexterityFTI
+from .fti import IConstructionFilter
+from Acquisition import aq_chain
+from Acquisition import aq_inner
+from euphorie.content.survey import ISurvey
 from plone.app.dexterity.behaviors.metadata import IBasic
 from plone.app.vocabularies.catalog import StaticCatalogVocabulary
 from plone.autoform import directives
@@ -9,7 +14,9 @@ from z3c.form.browser.select import SelectFieldWidget
 from z3c.relationfield.schema import RelationChoice
 from z3c.relationfield.schema import RelationList
 from zope import schema
+from zope.component import adapter
 from zope.interface import implementer
+from zope.interface import Interface
 
 
 class IChoice(model.Schema, IRichDescription, IBasic):
@@ -49,3 +56,29 @@ class Choice(Container):
         ]
         option_paths = ["/".join(obj.getPhysicalPath()[-3:]) for obj in option_objs]
         return "|".join(option_paths)
+
+
+@adapter(ConditionalDexterityFTI, Interface)
+@implementer(IConstructionFilter)
+class ConstructionFilter:
+    """FTI construction filter for :py:class:`Choice` objects. This filter makes sure
+    that Choice objects can only be added to tools of a `tool_type` that supports them.
+
+    This multi adapter requires the use of the conditional FTI as implemented
+    by :py:class:`euphorie.content.fti.ConditionalDexterityFTI`.
+    """
+
+    def __init__(self, fti, container):
+        self.fti = fti
+        self.container = container
+
+    def allowed(self):
+        """A choice is allowed to be created if the `tool_type` supports it.
+
+        :rtype: bool
+        """
+        for parent in aq_chain(aq_inner(self.container)):
+            if ISurvey.providedBy(parent):
+                return parent.get_tool_type_info()["allow_choice"]
+        # If we're not inside a survey we don't care what happens
+        return True
