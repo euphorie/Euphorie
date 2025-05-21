0ae278054314f890920b1fbd3fe05f1b4b6a6f26
ale-rt <alessandro.pisa@gmail.com>
2025-05-21 11:11:39 +0200

    Fix the default language when installing Euphorie
    When installing Euphorie the default language is set to en-us.
    This language is not listed in the available languages.
    This can cause the site to return errors when creating new objects that
    contain the language field.

---
+++ src/euphorie/upgrade/deployment/v18/20250521110138_fix_default_language/upgrade.py
@@ -0,0 +1,28 @@
+from ftw.upgrade import UpgradeStep
+from plone import api
+
+
+class FixDefaultLanguage(UpgradeStep):
+    """Fix default language."""
+
+    def __call__(self):
+        default_language = api.portal.get_registry_record("plone.default_language")
+        available_languages = api.portal.get_registry_record(
+            "plone.available_languages"
+        )
+        if default_language not in available_languages:
+
+            if "en" in available_languages:
+                # Set the default language to English if it's available
+                api.portal.set_registry_record("plone.default_language", "en")
+            elif available_languages and len(available_languages) == 1:
+                # Set the default language to the first available language
+                api.portal.set_registry_record(
+                    "plone.default_language", available_languages[0]
+                )
+            else:
+                raise ValueError(
+                    f"Cannot safely replace the default language {default_language}. "
+                    f"Please set-up manually the registry records "
+                    f"`plone.default_language` and `plone.available_languages`."
+                )
