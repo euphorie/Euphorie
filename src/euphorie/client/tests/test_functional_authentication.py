9a0121f1f8cd673efdf7de239872af6d7ef846c5
ale-rt <alessandro.pisa@gmail.com>
2025-06-10 11:38:42 +0200

    Allow the backend users to authenticate on the client

--- src/euphorie/client/tests/test_functional_authentication.py
+++ src/euphorie/client/tests/test_functional_authentication.py
@@ -1,7 +1,11 @@
 from Acquisition import aq_base
+from euphorie.client import config
 from euphorie.client.interfaces import IClientSkinLayer
 from euphorie.client.model import Account
 from euphorie.testing import EuphorieIntegrationTestCase
+from plone import api
+from plone.app.testing import TEST_USER_NAME
+from plone.app.testing import TEST_USER_PASSWORD
 from plonetheme.nuplone.skin.interfaces import NuPloneSkin
 from z3c.saconfig import Session
 from zope.interface import alsoProvides
@@ -51,4 +55,118 @@ class AuthenticationTests(EuphorieIntegrationTestCase):
         self.assertEqual(
             request.response.headers["location"],
             "http://nohost/plone/@@login?came_from=http%3A%2F%2Fnohost#login",
+        )
+
+
+class AuthenticationPluginTests(EuphorieIntegrationTestCase):
+    """Check if our authentication plugin is able to authenticate backend users
+    also in the frontend.
+
+    This requires the user to have a valid email address.
+
+    The missing row in the SQL database will be created automatically
+    if the user has a valid email address.
+    """
+
+    def setUp(self):
+        super().setUp()
+        request = self.app.REQUEST
+        alsoProvides(request, IClientSkinLayer)
+
+    def _call_authenticateCredentials(self, credentials):
+        acl_users = api.portal.get_tool("acl_users")
+        euphorie_plugin = acl_users.euphorie
+        return euphorie_plugin.authenticateCredentials(credentials)
+
+    def test_authentication_backend_user_no_email(self):
+        credentials = {
+            "login": TEST_USER_NAME,
+            "password": TEST_USER_PASSWORD,
+            "extractor": "credentials_cookie_auth",
+        }
+
+        with self.assertLogs("euphorie.client.authentication") as cm:
+            self.assertIsNone(self._call_authenticateCredentials(credentials))
+
+        self.assertListEqual(
+            cm.output,
+            [
+                f"WARNING:euphorie.client.authentication:No email address set for user {TEST_USER_NAME!r}. Cannot create a SQL account."  # noqa: E501
+            ],
+        )
+
+    def test_authentication_backend_user_proper_email(self):
+        api.user.get(username=TEST_USER_NAME).setProperties(
+            {"email": "foo@example.com"}
+        )
+
+        credentials = {
+            "login": TEST_USER_NAME,
+            "password": TEST_USER_PASSWORD,
+            "extractor": "credentials_cookie_auth",
+        }
+        with self.assertLogs("euphorie.client.authentication") as cm:
+            self.assertTupleEqual(
+                ("1", "foo@example.com"),
+                self._call_authenticateCredentials(credentials),
+            )
+
+        self.assertListEqual(
+            cm.output,
+            [
+                "INFO:euphorie.client.authentication:A SQL account 'foo@example.com' was created for user 'test-user'.",  # noqa: E501
+            ],
+        )
+
+        session = Session()
+        self.assertEqual(
+            session.query(Account).filter_by(loginname="foo@example.com").one().id, 1
+        )
+        self.assertEqual(credentials["email_overrides_login"], "foo@example.com")
+
+    def test_authentication_backend_user_proper_email_account_already_existing(self):
+        api.user.get(username=TEST_USER_NAME).setProperties(
+            {"email": "foo@example.com"}
+        )
+
+        session = Session()
+        session.add(
+            Account(
+                loginname="foo@example.com",
+                tc_approved=1,
+                password=TEST_USER_PASSWORD,
+                account_type=config.FULL_ACCOUNT,
+            )
+        )
+
+        credentials = {
+            "login": TEST_USER_NAME,
+            "password": TEST_USER_PASSWORD,
+            "extractor": "credentials_cookie_auth",
+        }
+        self.assertTupleEqual(
+            ("1", "foo@example.com"),
+            self._call_authenticateCredentials(credentials),
+        )
+
+    def test_authentication_backend_user_bogus_password(self):
+        credentials = {
+            "login": TEST_USER_NAME,
+            "extractor": "credentials_cookie_auth",
+            "password": "malicious_password",
+        }
+        api.user.get(username=TEST_USER_NAME).setProperties(
+            {"email": "foo@example.com"}
+        )
+
+        with self.assertLogs("euphorie.client.authentication") as cm:
+            self.assertIsNone(
+                self._call_authenticateCredentials(credentials),
+            )
+
+        self.assertListEqual(
+            cm.output,
+            [
+                f"WARNING:euphorie.client.authentication:Credentials for user {TEST_USER_NAME!r} are not valid according to other plugins. Refusing to create a SQL account.",  # noqa: E501
+            ],
         )
