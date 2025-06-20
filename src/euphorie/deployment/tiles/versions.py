80a255c8ccd6a16554b4088b78b3eb07092ff856
Maurits van Rees <maurits@vanrees.org>
2025-06-20 13:46:26 +0200

    Versions tile: show links to survey on client side.
    Link to the currently published survey, and the preview survey, if they exist.

--- src/euphorie/deployment/tiles/versions.py
+++ src/euphorie/deployment/tiles/versions.py
@@ -2,11 +2,21 @@ from Acquisition import aq_chain
 from Acquisition import aq_inner
 from euphorie.content.sector import getSurveys
 from euphorie.content.sector import ISector
+from euphorie.content.utils import survey_client_url
+from functools import cached_property
 from plone.tiles import Tile
 from plonetheme.nuplone.utils import checkPermission
 
 
 class SurveyVersions(Tile):
+    @cached_property
+    def published_url(self):
+        return survey_client_url(self.context, must_exist=True)
+
+    @cached_property
+    def preview_url(self):
+        return survey_client_url(self.context, must_exist=True, preview=True)
+
     def update(self):
         for sector in aq_chain(aq_inner(self.context)):
             if ISector.providedBy(sector):
