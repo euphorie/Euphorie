815a4cdaa33c7666c2fcfc79a702da3e51af6826
Maurits van Rees <maurits@vanrees.org>
2025-09-30 13:17:40 +0200

    Add commented out patch for z3c.saconfig to NOT autoflush.
    This may not be what we want, but this is at least one way to restore our plone.protect auto csrf protection for SQLAlchemy sessions.

--- src/euphorie/patches/__init__.py
+++ src/euphorie/patches/__init__.py
@@ -1,6 +1,15 @@
 from plone.app.upgrade.v52 import final
 
 
+# from z3c.saconfig.utility import SESSION_DEFAULTS
+
+
+# TODO There should be nicer ways via zcml to do this,
+# but for now this gets the job done, so we can test with it.
+# SESSION_DEFAULTS["autoflush"] = False
+# print("WARNING: patched z3c.saconfig to NOT autoflush.")
+
+
 def rebuild_redirections(context):
     """For Euphorie does not have the registration tool (look for the
     disableRedirectTracking setuphandler), so we must skip this upgrade.
