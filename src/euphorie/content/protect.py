6fcb4964882ee560e7d3f3347ee34884076be119
Maurits van Rees <maurits@vanrees.org>
2025-09-30 13:56:02 +0200

    auto csrf protection: check for changes in SessionDataManager transaction.
    If the session was flushed, it does not contain info on dirty/new/deleted items.
    But that info *is* available on the transaction.

--- src/euphorie/content/protect.py
+++ src/euphorie/content/protect.py
@@ -1,6 +1,11 @@
 from plone.protect.auto import ProtectTransform
 from zope.sqlalchemy.datamanager import SessionDataManager
 
+import logging
+
+
+logger = logging.getLogger(__name__)
+
 
 class EuphorieProtectTransform(ProtectTransform):
     def _registered_objects(self):
@@ -24,10 +29,30 @@ class EuphorieProtectTransform(ProtectTransform):
                         print(f"            dirty? {resource.session.dirty}")
                         print(f"            new? {resource.session.new}")
                         print(f"            deleted? {resource.session.deleted}")
+                        print("        Resource TRANSACTION:")
+                        print(f"             dirty? {bool(resource.tx._dirty)}")
+                        print(f"             new? {bool(resource.tx._new)}")
+                        print(f"             deleted? {bool(resource.tx._deleted)}")
                     orig = len(registered)
                     registered.extend(resource.session.dirty)
                     registered.extend(resource.session.new)
                     registered.extend(resource.session.deleted)
+                    # Some changes may have been flushed already, but not yet
+                    # committed.  This is because in z3c.saconfig by default
+                    # autoflush is true.  This means you get a flush on every query.
+                    # Solution (hopefully): check the dirty/new/deleted state
+                    # of the transaction object of the resource.
+                    try:
+                        registered.extend(resource.tx._dirty)
+                        registered.extend(resource.tx._new)
+                        registered.extend(resource.tx._deleted)
+                    except AttributeError:
+                        # We access private attributes, so let's catch exceptions.
+                        # Otherwise valid transactions may be aborted.
+                        logger.warning(
+                            "Could not access private attributes of resource.tx."
+                        )
+
                     if len(registered) != orig:
                         print(f"      added {len(registered) - orig} objects")
                     else:
