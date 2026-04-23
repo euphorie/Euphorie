22dbd34e619751a79d41bc1a8a8037601684de7d
Maurits van Rees <maurits@vanrees.org>
2026-04-23 13:20:43 +0200

    User preferences: support setting language preferences.
    Add related registry settings: allow modifying language preferences and allow modifying content language preference.

--- src/euphorie/upgrade/deployment/v19/20260413000000_update_language_settings/upgrade.py
+++ src/euphorie/upgrade/deployment/v19/20260413000000_update_language_settings/upgrade.py
@@ -2,7 +2,10 @@ from ftw.upgrade import UpgradeStep
 
 
 class UpdateLanguageSettings(UpgradeStep):
-    """Update language settings: use_request_negotiation=True."""
+    """Update language settings: use_request_negotiation=True.
+
+    And allow to modify language preferences in the settings panel.
+    """
 
     def __call__(self):
         self.install_upgrade_profile()
